3862a6de7e4dffc46bfbd76afa3bab537c577964
Charles Xue <charlesx2013@gmail.com>
2015-09-22 16:16:56 -0700

    added crashlytics

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/MainActivity.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
 

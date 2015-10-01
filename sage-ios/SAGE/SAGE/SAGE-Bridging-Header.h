0d0c2c955591934d626932936df684699a152c7c
Andrew Millman <andrewmillman35@gmail.com>
2015-10-01 12:59:10 -0700

    Add bridging header file

---
+++ sage-ios/SAGE/SAGE/SAGE-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  SAGE-Bridging-Header.h
+//  SAGE
+//
+//  Created by Andrew on 10/1/15.
+//  Copyright © 2015 Cal Blueprint. All rights reserved.
+//
+
+#import <AFNetworking/AFNetworking.h>
+#import <FontAwesomeKit/FontAwesomeKit.h>
+#import <Mantle/Mantle.h>

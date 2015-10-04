330c8ce9915b46d8e256d6f455e01c9faecb8162
Andrew Millman <andrewmillman35@gmail.com>
2015-10-03 20:27:16 -0700

    Style fixes

--- sage-ios/SAGE/SAGE/UIConstants.swift
+++ sage-ios/SAGE/SAGE/UIConstants.swift
@@ -10,14 +10,14 @@ import Foundation
 
 struct UIConstants {
     
-    static let profileImageSize : CGFloat = 40.0
-    static let sideMargin : CGFloat = 15.0
-    static let verticalMargin : CGFloat = 10.0
-    static let textMargin : CGFloat = 10.0
-    static let navbarHeight : CGFloat = 64.0
-    static let barbuttonSize : CGFloat = 44.0
-    static let barbuttonIconSize : CGFloat = 24.0
-    static let tabBarIconSize : CGFloat = 28.0
+    static let profileImageSize: CGFloat = 40.0
+    static let sideMargin: CGFloat = 15.0
+    static let verticalMargin: CGFloat = 10.0
+    static let textMargin: CGFloat = 10.0
+    static let navbarHeight: CGFloat = 64.0
+    static let barbuttonSize: CGFloat = 44.0
+    static let barbuttonIconSize: CGFloat = 24.0
+    static let tabBarIconSize: CGFloat = 28.0
     
     static func dividerHeight() -> CGFloat {
         return 1 / UIScreen.mainScreen().scale

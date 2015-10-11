926609f6e50f35f785cbdd697c462d9a0b5f6eb7
Kelsey Lam <kelseylam@berkeley.edu>
2015-10-10 20:53:01 -0700

    created models/announcements and started networks/announcements/announcementindex

---
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/models/Announcement.java
@@ -0,0 +1,17 @@
+package blueprint.com.sage.models;
+
+import java.util.Date;
+
+/**
+ * Created by kelseylam on 10/10/15.
+ */
+@Data
+public class Announcement {
+    private int mId;
+    private Date mCreatedAt;
+    private Date mUpdatedAt;
+    private String mTitle;
+    private int mSchoolId;
+    private int mUserId;
+    private int mCategory;
+}

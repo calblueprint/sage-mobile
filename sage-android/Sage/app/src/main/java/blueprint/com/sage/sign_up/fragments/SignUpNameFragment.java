4f8b85d42be0144999f9c1a11d93a891a53e0957
Charles Xue <charlesx2013@gmail.com>
2015-10-14 00:22:13 -0700

    moving fragments to abstract, and validations for name

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/sign_up/fragments/SignUpNameFragment.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/sign_up/fragments/SignUpNameFragment.java
@@ -1,18 +1,22 @@
 package blueprint.com.sage.sign_up.fragments;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import blueprint.com.sage.R;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
  * Created by charlesx on 10/12/15.
  */
-public class SignUpNameFragment extends Fragment {
+public class SignUpNameFragment extends SignInAbstractFragment {
+
+    @Bind(R.id.sign_up_first_name) EditText mFirstName;
+    @Bind(R.id.sign_up_last_name) EditText mLastName;
 
     public static SignUpNameFragment newInstance() { return new SignUpNameFragment(); }
 
@@ -26,6 +30,49 @@ public class SignUpNameFragment extends Fragment {
         super.onCreateView(inflater, parent, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_sign_up_name, parent, false);
         ButterKnife.bind(this, view);
+        initializeViews();
         return view;
+    }
+
+    private void initializeViews() {
+        String firstName = getParentActivity().getUser().getFirstName();
+        if (firstName != null && !firstName.isEmpty()) {
+            mFirstName.setText(firstName);
+        }
+
+        String lastName = getParentActivity().getUser().getLastName();
+        if (lastName != null && !lastName.isEmpty()) {
+            mLastName.setText(lastName);
+        }
+    }
+
+    public boolean hasValidFields() {
+        boolean isValid = true;
+
+        if (!hasValidFirstName()) {
+            mFirstName.setError(getString(R.string.cannot_be_blank,
+                                getString(R.string.sign_up_first_name)));
+            isValid = false;
+        }
+
+        if (!hasValidLastName()) {
+            mLastName.setError(getString(R.string.cannot_be_blank,
+                               getString(R.string.sign_up_last_name)));
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
+    private boolean hasValidFirstName() {
+        return !mFirstName.getText().toString().isEmpty();
+    }
+
+    private boolean hasValidLastName() {
+        return !mLastName.getText().toString().isEmpty();
+    }
+
+    public void contineToNextPage() {
+
     }
 }

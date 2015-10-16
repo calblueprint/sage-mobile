5fc6f6255a2a86b8c912fd9c97dea1581acda1d8
Charles Xue <charlesx2013@gmail.com>
2015-10-15 22:57:53 -0700

    fixing school and user request, added loggin in

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/sign_up/SignUpActivity.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/sign_up/SignUpActivity.java
@@ -1,11 +1,23 @@
 package blueprint.com.sage.sign_up;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import com.android.volley.Response;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import blueprint.com.sage.R;
+import blueprint.com.sage.models.School;
+import blueprint.com.sage.models.Session;
 import blueprint.com.sage.models.User;
+import blueprint.com.sage.network.schools.SchoolListRequest;
+import blueprint.com.sage.network.users.CreateUserRequest;
 import blueprint.com.sage.sign_up.fragments.SignUpPagerFragment;
+import blueprint.com.sage.utility.network.NetworkManager;
 import blueprint.com.sage.utility.view.FragUtil;
 
 /**
@@ -15,13 +27,70 @@ import blueprint.com.sage.utility.view.FragUtil;
 public class SignUpActivity extends FragmentActivity {
 
     private User mUser;
+    private List<School> mSchools;
+
+    private SharedPreferences mPreferences;
+    private String mPreferenceTag;
+
+    private NetworkManager mManager;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
         mUser = new User();
+        mSchools = new ArrayList<>();
+
+        mPreferenceTag = getString(R.string.shared_preferences);
+        mPreferences = getSharedPreferences(mPreferenceTag, MODE_PRIVATE);
+
+        makeSchoolRequest();
         FragUtil.replace(R.id.sign_up_container, SignUpPagerFragment.newInstance(), this);
+    }
+
+    private void makeSchoolRequest() {
+        SchoolListRequest request = new SchoolListRequest(this,
+            new Response.Listener<ArrayList<School>>() {
+                @Override
+                public void onResponse(ArrayList<School> schools) {
+                    setSchools(schools);
+                }
+            },
+            new Response.Listener() {
+                @Override
+                public void onResponse(Object o) {
+                    Log.e(getClass().toString(), o.toString());
+            }
+        });
+
+        mManager.getRequestQueue().add(request);
+    }
+
+    private void makeUserRequest() {
+        CreateUserRequest request = new CreateUserRequest(this, getUser(),
+                new Response.Listener<Session>() {
+                    @Override
+                    public void onResponse(Session session) {
+                        logInUser(session);
+                    }
+                },
+                new Response.Listener() {
+                    @Override
+                    public void onResponse(Object o) {
+                       Log.e(getClass().toString(), o.toString());
+                    }
+                });
+        mManager.getRequestQueue().add(request);
+    }
+
+    private void logInUser(Session session) {
+        SharedPreferences.Editor editPreference =  mPreferences.edit();
+        editPreference.putString(getString(R.string.shared_preference_email), session.getEmail());
+        editPreference.putString(getString(R.string.shared_preference_auth_token),
+                                           session.getAuthenticationToken());
+        editPreference.apply();
+
+        // Start activity here
     }
 
     public User getUser() {

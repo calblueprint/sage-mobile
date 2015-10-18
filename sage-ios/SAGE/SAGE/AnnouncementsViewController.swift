fff947cbc2a8f9d7b5f132710754ff7c698ef9a7
Erica <eriicaericaerica@gmail.com>
2015-10-17 19:09:37 -0700

    somem table view stuff

--- sage-ios/SAGE/SAGE/AnnouncementsViewController.swift
+++ sage-ios/SAGE/SAGE/AnnouncementsViewController.swift
@@ -9,11 +9,43 @@
 import Foundation
 
 class AnnouncementsViewController: UIViewController {
+    
+    override func loadView() {
+        self.view = AnnouncementsView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.whiteColor()
         self.title = "Announcements"
+        (self.view as! AnnouncementsView).tableView.delegate = self
+        (self.view as! AnnouncementsView).tableView.dataSource = self
     }
 
+}
+
+extension AnnouncementsViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 44.0
+    }
+}
+
+extension AnnouncementsViewController: UITableViewDataSource {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var tableViewCell = tableView.dequeueReusableCellWithIdentifier("Announcement")
+        if (tableViewCell == nil) {
+            tableViewCell = UITableViewCell(style: .Default, reuseIdentifier: "Announcement")
+        }
+        tableViewCell!.textLabel?.text = "Ass"
+        return tableViewCell!
+    }
+    
 }

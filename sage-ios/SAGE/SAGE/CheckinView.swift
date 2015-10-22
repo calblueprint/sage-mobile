4ca088f0e0a94c4aa0fb74197432eb4c8127bcbe
Andrew Millman <andrewmillman35@gmail.com>
2015-10-21 23:50:33 -0700

    Add timer arc

--- sage-ios/SAGE/SAGE/CheckinView.swift
+++ sage-ios/SAGE/SAGE/CheckinView.swift
@@ -13,9 +13,11 @@ class CheckinView: UIView {
     var startButton: UIButton = UIButton()
     var endButton: UIButton = UIButton()
     var timerView: UIView = UIView()
+    var timerArc: CAShapeLayer = CAShapeLayer()
     var mapView: GMSMapView = GMSMapView()
     
     private let buttonSize: CGFloat = 56.0
+    private let timerSize: CGFloat = 80.0
     private let margin: CGFloat = 10.0
     private let shadowOpacity: Float = 0.5
 
@@ -52,11 +54,20 @@ class CheckinView: UIView {
     // MARK: - Public methods
     //
     func presentDefaultMode(duration: NSTimeInterval) {
-        self.showButton(self.startButton, hide: self.endButton, duration: duration)
+        self.showView(self.startButton, hide: self.endButton, duration: duration)
+        UIView.animateWithDuration(duration) { () -> Void in
+            self.timerView.alpha = 0
+        }
     }
     
     func presentSessionMode(duration: NSTimeInterval) {
-        self.showButton(self.endButton, hide: self.startButton, duration: duration)
+        self.showView(self.endButton, hide: self.startButton, duration: duration)
+        self.showView(self.timerView, hide: nil, duration: duration)
+        self.updateTimerWithTime(20, completionTime: 20)
+    }
+    
+    func updateTimerWithTime(time: NSTimeInterval, completionTime: NSTimeInterval) {
+        self.timerArc.path = self.createTimerArcWithPercentage(0.20)
     }
     
     //
@@ -86,16 +97,19 @@ class CheckinView: UIView {
         self.addSubview(self.endButton)
         
         self.timerView.backgroundColor = UIColor.whiteColor()
-        self.timerView.setSize(width: 70, height: 70)
-        self.timerView.layer.cornerRadius = 35
+        self.timerView.setSize(width: timerSize, height: timerSize)
+        self.timerView.layer.cornerRadius = timerSize/2
+        self.timerView.layer.addSublayer(self.timerArc)
         self.addShadowToView(self.timerView)
         self.addSubview(self.timerView)
+        
+        self.timerArc.fillColor = UIColor.lightRedColor.CGColor
     }
     
-    private func showButton(buttonShow: UIButton, hide buttonHide: UIButton, duration: NSTimeInterval) {
-        buttonShow.transform = CGAffineTransformMakeScale(0.05, 0.05)
-        buttonShow.alpha = 1
-        self.bringSubviewToFront(buttonShow)
+    private func showView(showView: UIView, hide hideView: UIView?, duration: NSTimeInterval) {
+        showView.transform = CGAffineTransformMakeScale(0.05, 0.05)
+        showView.alpha = 1
+        self.bringSubviewToFront(showView)
         
         UIView.animateWithDuration(duration,
             delay: 0,
@@ -103,8 +117,8 @@ class CheckinView: UIView {
             initialSpringVelocity: UIConstants.defaultSpringVelocity,
             options:.CurveEaseInOut,
             animations: { () -> Void in
-                buttonShow.transform = CGAffineTransformIdentity
-                buttonHide.alpha = 0
+                showView.transform = CGAffineTransformIdentity
+                hideView?.alpha = 0
             }) { (complete: Bool) -> Void in }
     }
     
@@ -112,5 +126,24 @@ class CheckinView: UIView {
         view.layer.shadowOffset = CGSizeMake(0,1)
         view.layer.shadowRadius = 1.5
         view.layer.shadowOpacity = shadowOpacity
+    }
+    
+    private func createTimerArcWithPercentage(percentage: CGFloat) -> CGPath {
+        let pi: CGFloat = 3.1415926
+        let arc = CGPathCreateMutable()
+        CGPathMoveToPoint(arc, nil, timerSize/2, 0)
+        CGPathAddArc(arc,
+            nil,
+            timerSize/2, timerSize/2,   // center.x, center.y
+            timerSize/2,                // radius
+            -pi/2,                      // start angle
+            -pi/2 + 2*pi*percentage,    // end angle
+            false)                      // counter clockwise?
+        let strokedArc = CGPathCreateCopyByStrokingPath(arc, nil,
+            5.0,                        // lineWidth
+            .Round,                     // line cap
+            .Round,                     // edge join
+            10)                         // thickness
+        return strokedArc!
     }
 }

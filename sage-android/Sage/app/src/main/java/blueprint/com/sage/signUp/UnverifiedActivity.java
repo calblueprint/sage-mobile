1b8d9e2865780e6753f5be3ea5e9d85f33b6723b
Charles Xue <charlesx2013@gmail.com>
2015-10-24 20:50:15 -0700

    filling out unverified activity

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/signUp/UnverifiedActivity.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/signUp/UnverifiedActivity.java
@@ -1,23 +1,22 @@
 package blueprint.com.sage.signUp;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 
 import blueprint.com.sage.R;
+import blueprint.com.sage.shared.AbstractActivity;
+import butterknife.ButterKnife;
 
 /**
  * Created by charlesx on 10/24/15.
  * Activity shown when a user isn't verified yet.
  */
-public class UnverifiedActivity extends FragmentActivity {
+public class UnverifiedActivity extends AbstractActivity {
 
-    private SharedPreferences mPreferences;
+
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mPreferences = getSharedPreferences(getString(R.string.preferences), MODE_PRIVATE);
-
+        setContentView(R.layout.activity_unverified);
+        ButterKnife.bind(this);
     }
 }

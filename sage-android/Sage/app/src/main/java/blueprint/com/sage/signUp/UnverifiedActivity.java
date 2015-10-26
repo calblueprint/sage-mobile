f5500b8ff176ffd176dc7b23cce79de548ec17ff
Charles Xue <charlesx2013@gmail.com>
2015-10-26 16:52:23 -0700

    styling unverified activity

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/signUp/UnverifiedActivity.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/signUp/UnverifiedActivity.java
@@ -2,8 +2,12 @@ package blueprint.com.sage.signUp;
 
 import android.os.Bundle;
 
+import com.squareup.picasso.Picasso;
+
 import blueprint.com.sage.R;
 import blueprint.com.sage.shared.AbstractActivity;
+import blueprint.com.sage.shared.views.CircleImageView;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
@@ -12,11 +16,24 @@ import butterknife.ButterKnife;
  */
 public class UnverifiedActivity extends AbstractActivity {
 
-
+    @Bind(R.id.unverified_photo_circle) CircleImageView mImageView;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unverified);
         ButterKnife.bind(this);
+
+        initializeProfilePhoto();
+    }
+
+    private void initializeProfilePhoto() {
+
+        Picasso picasso = Picasso.with(this);
+
+        if (mUser.getImageUrl() == null) {
+            picasso.load(R.drawable.default_profile).into(mImageView);
+        } else {
+            picasso.load(mUser.getImageUrl()).into(mImageView);
+        }
     }
 }
--- sage-android/Sage/app/src/main/java/blueprint/com/sage/signin/SignInFragment.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/signin/SignInFragment.java
@@ -3,6 +3,7 @@ package blueprint.com.sage.signIn;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -96,6 +97,7 @@ public class SignInFragment extends Fragment {
         try {
             NetworkUtils.loginUser(session, getActivity());
         } catch(JsonProcessingException e) {
+            Log.e(getClass().toString(), e.toString());
             Toast.makeText(getActivity(), "Something went wrong, try again!", Toast.LENGTH_SHORT).show();
         }
     }

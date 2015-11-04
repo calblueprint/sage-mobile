0c070cc8d2db968f6b13a7531541b05ec335465d
Charles Xue <charlesx2013@gmail.com>
2015-11-04 00:30:07 -0800

    making the floating action text work

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/shared/views/FloatingTextView.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/shared/views/FloatingTextView.java
@@ -1,16 +1,19 @@
 package blueprint.com.sage.shared.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.design.widget.FloatingActionButton;
 import android.util.AttributeSet;
 
+import blueprint.com.sage.R;
 import blueprint.com.sage.utility.view.ViewUtils;
 
 /**
  * Created by charlesx on 11/3/15.
+ * Floating action button with text
  */
 public class FloatingTextView extends FloatingActionButton {
 
@@ -19,43 +22,56 @@ public class FloatingTextView extends FloatingActionButton {
 
     private Paint mTextPaint;
     private String mText;
+    private int mTextColor;
+    private int mTextSize;
 
-    public FloatingTextView(Context context) {
-        super(context);
-        initializeView();
-    }
+    public FloatingTextView(Context context) { super(context); }
 
     public FloatingTextView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        initializeView();
+        this(context, attrs, 0);
     }
 
     public FloatingTextView(Context context, AttributeSet attrs, int defAttrStyle) {
         super(context, attrs, defAttrStyle);
-        initializeView();
+        initializeAttrs(context, attrs);
     }
 
-    private void initializeView() {
+    private void initializeAttrs(Context context, AttributeSet attrs) {
         mTextPaint = new Paint();
-        mText = "00:00:00";
+
+        TypedArray attributes =
+                context.getTheme().obtainStyledAttributes(attrs, R.styleable.FloatingTextView, 0, 0);
+
+        mTextColor = attributes.getColor(R.styleable.FloatingTextView_android_textColor, Color.BLACK);
+        String text = attributes.getString(R.styleable.FloatingTextView_android_text);
+        if (text != null)
+            mText = text;
+        mTextSize = attributes.getDimensionPixelSize(R.styleable.FloatingTextView_android_textSize, TEXT_SIZE_DP);
+
+        attributes.recycle();
     }
 
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        if (mText == null)
+            return;
+
         mTextPaint.setTextAlign(Paint.Align.CENTER);
-        mTextPaint.setColor(Color.BLACK);
+        mTextPaint.setColor(mTextColor);
         mTextPaint.setAntiAlias(true);
 
-        float textSize = ViewUtils.convertFromDpToPx(getContext(), TEXT_SIZE_DP);
-        mTextPaint.setTextSize(textSize);
-        float x = getX();
-        float y = getY();
+        mTextPaint.setTextSize(mTextSize);
 
         int xPos = (canvas.getWidth() / 2);
         int yPos = (int) ((canvas.getHeight() / 2) - ((mTextPaint.descent() + mTextPaint.ascent()) / 2)) ;
 
         canvas.drawText(mText, xPos, yPos, mTextPaint);
+    }
+
+    public void setText(String text) {
+        mText = text;
+        invalidate();
     }
 }

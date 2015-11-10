bb810a6d200f99d41676bb5aa972e7f31a9664bb
Charles Xue <charlesx2013@gmail.com>
2015-11-10 13:19:43 -0800

    add requests to activity

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/checkIn/CheckInListActivity.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/checkIn/CheckInListActivity.java
@@ -2,6 +2,15 @@ package blueprint.com.sage.checkIn;
 
 import android.os.Bundle;
 
+import com.android.volley.Response;
+
+import java.util.ArrayList;
+
+import blueprint.com.sage.models.APIError;
+import blueprint.com.sage.models.CheckIn;
+import blueprint.com.sage.network.check_ins.CheckInListRequest;
+import blueprint.com.sage.network.check_ins.DeleteCheckInRequest;
+import blueprint.com.sage.network.check_ins.VerifyCheckInRequest;
 import blueprint.com.sage.shared.NavigationAbstractActivity;
 
 /**
@@ -9,8 +18,64 @@ import blueprint.com.sage.shared.NavigationAbstractActivity;
  */
 public class CheckInListActivity extends NavigationAbstractActivity {
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        makeCheckInListRequest();
+    }
+
+    public void makeCheckInListRequest() {
+        CheckInListRequest request = new CheckInListRequest(this,
+            new Response.Listener<ArrayList<CheckIn>>() {
+                @Override
+                public void onResponse(ArrayList<CheckIn> checkIns) {
+
+                }
+            },
+            new Response.Listener<APIError>() {
+                @Override
+                public void onResponse(APIError error) {
+
+            }
+        });
+
+        getNetworkManager().getRequestQueue().add(request);
+    }
+
+    public void makeDeleteCheckInRequest(CheckIn checkIn) {
+        DeleteCheckInRequest request = new DeleteCheckInRequest(this, checkIn,
+                new Response.Listener<CheckIn>() {
+                    @Override
+                    public void onResponse(CheckIn checkIn) {
+
+                    }
+                },
+                new Response.Listener<APIError>() {
+                    @Override
+                    public void onResponse(APIError error) {
+
+                    }
+                });
+
+        getNetworkManager().getRequestQueue().add(request);
+    }
+
+    public void makeVerifyCheckInRequest(CheckIn checkIn) {
+        VerifyCheckInRequest request = new VerifyCheckInRequest(this, checkIn,
+                new Response.Listener<CheckIn>() {
+                    @Override
+                    public void onResponse(CheckIn checkIn) {
+
+                    }
+                },
+                new Response.Listener<APIError>() {
+                    @Override
+                    public void onResponse(APIError error) {
+
+                    }
+                });
+
+        getNetworkManager().getRequestQueue().add(request);
     }
 }

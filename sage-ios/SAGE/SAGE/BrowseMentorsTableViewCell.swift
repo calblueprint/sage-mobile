81d29ece04a0a5ccbdfa7780cbcbcd995063360b
Sameera Vemulapalli <sameera.vemulapalli@berkeley.edu>
2015-11-10 17:59:48 -0800

    added basic tableview

---
+++ sage-ios/SAGE/SAGE/BrowseMentorsTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BrowseMentorsTableViewCell.swift
+//  SAGE
+//
+//  Created by Sameera Vemulapalli on 11/9/15.
+//  Copyright © 2015 Cal Blueprint. All rights reserved.
+//
+
+import UIKit
+
+class BrowseMentorsTableViewCell: UITableViewCell {
+
+}

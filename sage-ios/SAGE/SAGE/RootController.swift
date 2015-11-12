40b07df6b939b740f67e59a4b2e2123fc19c499d
Andrew Millman <andrewmillman35@gmail.com>
2015-11-11 19:53:24 -0800

    Move viewDidAppear logic for RootController to viewDidLoad

--- sage-ios/SAGE/SAGE/RootController.swift
+++ sage-ios/SAGE/SAGE/RootController.swift
@@ -10,24 +10,6 @@ import UIKit
 
 class RootController: UIViewController {
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        if LoginHelper.userIsLoggedIn() {
-            let completion = { (success: Bool) -> Void in
-               dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    if success {
-                        self.pushRootTabBarController()
-                    } else {
-                        self.pushUnverifiedViewController()
-                    }
-               })
-            }
-            LoginHelper.userIsVerified(completion)
-        } else {
-            self.pushLoginViewController()
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         let frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height)
@@ -50,6 +32,21 @@ class RootController: UIViewController {
         sageLabel.textColor = UIColor.whiteColor()
         sageLabel.font = UIFont.getDefaultFont(20)
         self.view.addSubview(sageLabel)
+        
+        if LoginHelper.userIsLoggedIn() {
+            let completion = { (success: Bool) -> Void in
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    if success {
+                        self.pushRootTabBarController()
+                    } else {
+                        self.pushUnverifiedViewController()
+                    }
+                })
+            }
+            LoginHelper.userIsVerified(completion)
+        } else {
+            self.pushLoginViewController()
+        }
     }
     
     // a function that will fill in values and present the login view controller

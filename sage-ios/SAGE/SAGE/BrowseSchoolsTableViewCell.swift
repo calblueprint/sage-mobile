a00b6a0537395af2d00008a47e03390d1fed6ec7
Sameera Vemulapalli <sameera.vemulapalli@berkeley.edu>
2015-11-19 02:02:39 -0800

    set up everything for network requests, set up the section index titles

---
+++ sage-ios/SAGE/SAGE/BrowseSchoolsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BrowseSchoolsTableViewCell.swift
+//  SAGE
+//
+//  Created by Sameera Vemulapalli on 11/19/15.
+//  Copyright © 2015 Cal Blueprint. All rights reserved.
+//
+
+import UIKit
+
+class BrowseSchoolsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

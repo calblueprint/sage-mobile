44e423650388df9effd85f3162195405bd515135
Charles Xue <charlesx2013@gmail.com>
2015-11-23 21:48:46 -0800

    convert to interface pt2

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/checkIn/CheckInActivity.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/checkIn/CheckInActivity.java
@@ -3,16 +3,20 @@ package blueprint.com.sage.checkIn;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import blueprint.com.sage.R;
 import blueprint.com.sage.checkIn.fragments.CheckInMapFragment;
 import blueprint.com.sage.shared.activities.NavigationAbstractActivity;
+import blueprint.com.sage.shared.interfaces.CheckInInterface;
 import blueprint.com.sage.utility.view.FragUtils;
 
 /**
  * Created by charlesx on 10/16/15.
  * Check in Activity
  */
-public class CheckInActivity extends NavigationAbstractActivity {
+public class CheckInActivity extends NavigationAbstractActivity implements CheckInInterface {
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {

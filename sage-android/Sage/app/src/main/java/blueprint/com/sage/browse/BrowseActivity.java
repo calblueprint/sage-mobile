2d94f3e9236813e1b904a9abfb364875b55f1e4f
Charles Xue <charlesx2013@gmail.com>
2015-11-23 21:29:49 -0800

    changing everything to interfaces pt 1

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/browse/BrowseActivity.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/browse/BrowseActivity.java
@@ -12,12 +12,13 @@ import blueprint.com.sage.models.User;
 import blueprint.com.sage.network.Requests;
 import blueprint.com.sage.shared.activities.NavigationAbstractActivity;
 import blueprint.com.sage.shared.interfaces.SchoolsInterface;
+import blueprint.com.sage.shared.interfaces.UsersInterface;
 import blueprint.com.sage.utility.view.FragUtils;
 
 /**
  * Created by charlesx on 11/4/15.
  */
-public class BrowseActivity extends NavigationAbstractActivity implements SchoolsInterface {
+public class BrowseActivity extends NavigationAbstractActivity implements SchoolsInterface, UsersInterface {
 
     private List<School> mSchools;
     private List<User> mUsers;

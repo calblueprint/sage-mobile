ceee92544a6d956dc8bd7e4ae6d389a1e5b6abd7
Charles Xue <charlesx2013@gmail.com>
2015-11-29 19:27:34 -0800

    added abstract fragment for edit/create school

---
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/browse/fragments/EditSchoolFragment.java
@@ -0,0 +1,25 @@
+package blueprint.com.sage.browse.fragments;
+
+import blueprint.com.sage.models.School;
+
+/**
+ * Created by charlesx on 11/29/15.
+ */
+public class EditSchoolFragment extends SchoolFormAbstractFragment {
+
+    public static EditSchoolFragment newInstance(School school) {
+        EditSchoolFragment fragment = new EditSchoolFragment();
+        fragment.setSchool(school);
+        return fragment;
+    }
+
+    public void setSchool(School school) { mSchool = school; }
+
+    public void initializeSchool() {
+        mSchoolName.setText(mSchool.getName());
+        mSchoolAddress.setText(mSchool.getAddress());
+
+        mNavigationInterface.toggleDrawerUse(false);
+        getActivity().setTitle("Edit School");
+    }
+}

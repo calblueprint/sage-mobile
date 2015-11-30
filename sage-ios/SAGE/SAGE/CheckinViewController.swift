e50460caec2966b4c9613f76df98e575f6ac20d7
Andrew Millman <andrewmillman35@gmail.com>
2015-11-29 19:05:16 -0800

    Replace add icon with default

--- sage-ios/SAGE/SAGE/CheckinViewController.swift
+++ sage-ios/SAGE/SAGE/CheckinViewController.swift
@@ -40,7 +40,7 @@ class CheckinViewController: UIViewController {
 
         self.setupDefaultTitleLabel()
         self.setupSessionTitleLabel()
-        self.setupBarButtonItems()
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "presentRequestHoursView")
         
         self.checkinView.startButton.addTarget(self, action: "userPressedBeginSession", forControlEvents: .TouchUpInside)
         self.checkinView.endButton.addTarget(self, action: "userPressedEndSession", forControlEvents: .TouchUpInside)
@@ -197,16 +197,6 @@ class CheckinViewController: UIViewController {
         self.sessionTitleLabel.sizeToFit()
         self.sessionTitleLabel.frame = CGRectIntegral(self.sessionTitleLabel.frame) // Done to prevent fuzzy text
         self.sessionTitleLabel.centerInSuperview()
-    }
-    
-    private func setupBarButtonItems() {
-        let increase: CGFloat = 2.0
-        let requestIcon = FAKMaterialIcons.plusIconWithSize(UIConstants.barbuttonIconSize + increase)
-        let requestImage = requestIcon.imageWithSize(CGSizeMake(
-            UIConstants.barbuttonIconSize + increase,
-            UIConstants.barbuttonIconSize + increase))
-        let requestHoursItem = UIBarButtonItem(image: requestImage!, style: .Plain, target: self, action: "presentRequestHoursView")
-        self.navigationItem.rightBarButtonItem = requestHoursItem
     }
     
     private func startGettingCurrentLocation() {

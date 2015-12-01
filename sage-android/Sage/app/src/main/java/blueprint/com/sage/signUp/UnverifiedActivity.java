31cf85d9fce648a60af5fdcbfc5d0f7ca02bd511
Charles Xue <charlesx2013@gmail.com>
2015-12-01 08:32:20 -0800

    refresh on page load to see if user was verified

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/signUp/UnverifiedActivity.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/signUp/UnverifiedActivity.java
@@ -1,13 +1,19 @@
 package blueprint.com.sage.signUp;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import blueprint.com.sage.R;
+import blueprint.com.sage.events.users.UserEvent;
+import blueprint.com.sage.models.User;
+import blueprint.com.sage.network.Requests;
 import blueprint.com.sage.shared.activities.AbstractActivity;
 import blueprint.com.sage.shared.views.CircleImageView;
+import blueprint.com.sage.utility.network.NetworkUtils;
 import blueprint.com.sage.utility.view.ViewUtils;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import de.greenrobot.event.EventBus;
 
 /**
  * Created by charlesx on 10/24/15.
@@ -17,6 +23,7 @@ public class UnverifiedActivity extends AbstractActivity {
 
     @Bind(R.id.unverified_photo_circle) CircleImageView mImageView;
 
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unverified);
@@ -24,6 +31,19 @@ public class UnverifiedActivity extends AbstractActivity {
 
         initializeViews();
         initializeProfilePhoto();
+        refreshUser();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
     }
 
     private void initializeViews() {
@@ -32,5 +52,22 @@ public class UnverifiedActivity extends AbstractActivity {
 
     private void initializeProfilePhoto() {
         mUser.loadUserImage(this, mImageView);
+    }
+
+    public void refreshUser() {
+        Requests.Users.with(this).makeShowRequest(mUser);
+    }
+
+    public void onEvent(UserEvent event) { checkUser(event.getUser()); }
+
+    private void checkUser(User user) {
+        if (user.isVerified()) {
+            try {
+                NetworkUtils.setUser(this, user);
+                NetworkUtils.loginUser(this);
+            } catch (Exception e) {
+                Log.e(getClass().toString(), e.toString());
+            }
+        }
     }
 }

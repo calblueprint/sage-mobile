037348c7be42ac266e8c55882b3e604b387cfb53
Erica <eriicaericaerica@gmail.com>
2015-11-30 16:11:21 -0800

    remove hard coded numbers

--- sage-ios/SAGE/SAGE/ProfileView.swift
+++ sage-ios/SAGE/SAGE/ProfileView.swift
@@ -26,6 +26,14 @@ class ProfileView: UIView {
     let userCommitmentContainer = UIView()
     let userCommitmentLabel = UILabel()
     
+    // profile page constants
+    let leftMargin = CGFloat(30)
+    let headerHeight = CGFloat(120)
+    let titleFontSize = CGFloat(22)
+    let profileImageSize = CGFloat(90)
+    let profileImageBorder = CGFloat(6)
+    let boxHeight = CGFloat(100)
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addSubview(self.profileContent)
@@ -50,122 +58,128 @@ class ProfileView: UIView {
     }
     
     func setupWithUser(user: User) {
-        userName.text = user.firstName! + " " + user.lastName!
-        userSchool.text = user.school?.name
-        userVolunteerLevel.text = user.volunteerLevelToString(user.level)
+        self.profileUserImg.setImageWithUser(user)
+        
+        self.userName.text = user.fullName()
+        self.userSchool.text = user.school?.name
+        self.userVolunteerLevel.text = user.volunteerLevelToString(user.level)
         
         let boldString = [NSFontAttributeName: UIFont.getSemiboldFont(30)]
         
         let hoursCompleted = user.totalHours
-        let userStatusString = NSMutableAttributedString(string: String(hoursCompleted) + " / 60 \nhours")
+        let userStatusString = NSMutableAttributedString(string: String(hoursCompleted) + " / 60 \nhours") // ask charles about the 60
         userStatusString.addAttributes(boldString, range: NSRange(location: 0, length: 2))
         userStatusString.addAttribute(NSForegroundColorAttributeName, value: UIColor.secondaryTextColor, range: NSRange(location: 8, length: 6))
-        userStatusLabel.attributedText = userStatusString
-        userStatusLabel.sizeToFit()
-        userStatusLabel.centerInSuperview()
+        self.userStatusLabel.attributedText = userStatusString
+        self.userStatusLabel.sizeToFit()
+        self.userStatusLabel.centerInSuperview()
         
-        let weeklyHoursRequired = 2 // ask charles about this
+        let weeklyHoursRequired = user.getRequiredHours()
         let userCommitmentString = NSMutableAttributedString(string: String(weeklyHoursRequired) + " \nhours/week")
         userCommitmentString.addAttributes(boldString, range: NSRange(location: 0, length: 2))
         userCommitmentString.addAttribute(NSForegroundColorAttributeName, value: UIColor.secondaryTextColor, range: NSRange(location: 3, length: 10))
-        userCommitmentLabel.attributedText = userCommitmentString
-        userCommitmentLabel.sizeToFit()
-        userCommitmentLabel.centerInSuperview()
+        self.userCommitmentLabel.attributedText = userCommitmentString
+        self.userCommitmentLabel.sizeToFit()
+        self.userCommitmentLabel.centerInSuperview()
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        profileContent.setHeight(400)
-        profileContent.fillWidth()
+        self.profileContent.fillHeight()
+        self.profileContent.fillWidth()
         
         // set up header
-        header.setHeight(120)
-        header.fillWidth()
-        header.backgroundColor = UIColor.mainColor
+        self.header.setHeight(self.headerHeight)
+        self.header.fillWidth()
+        self.header.backgroundColor = UIColor.mainColor
         
         // position edit button
-        profileEditButton.text = "Edit Profile"
-        profileEditButton.font = UIFont.normalFont
-        profileEditButton.textColor = UIColor.secondaryTextColor
-        profileEditButton.setWidth(200)
-        profileEditButton.setHeight(14)
-        profileEditButton.setX(270)
-        profileEditButton.setY(135)
+        self.profileEditButton.text = "Edit Profile"
+        self.profileEditButton.font = UIFont.normalFont
+        self.profileEditButton.textColor = UIColor.secondaryTextColor
+        self.profileEditButton.sizeToFit()
+        let profileEditButtonX = CGRectGetWidth(self.header.frame) - self.leftMargin - CGRectGetWidth(self.profileEditButton.frame)
+        self.profileEditButton.setX(profileEditButtonX)
+        self.profileEditButton.setY(self.headerHeight + 15)
         
         // set up image
-        profileUserImgBorder.setHeight(96)
-        profileUserImgBorder.setWidth(96)
-        profileUserImgBorder.setY(60)
-        profileUserImgBorder.setX(25)
-        profileUserImgBorder.layer.cornerRadius = 96/2
-        profileUserImgBorder.backgroundColor = UIColor.whiteColor()
+        self.profileUserImgBorder.setHeight(self.profileImageSize + self.profileImageBorder)
+        self.profileUserImgBorder.setWidth(self.profileImageSize + self.profileImageBorder)
+        self.profileUserImgBorder.setY(self.headerHeight/2)
+        self.profileUserImgBorder.setX(self.leftMargin)
+        self.profileUserImgBorder.layer.cornerRadius = (self.profileImageSize + self.profileImageBorder)/2
+        self.profileUserImgBorder.backgroundColor = UIColor.whiteColor()
         
-        let image = UIImage(named: "UserImage.jpg")
-        profileUserImg.image = image
-        profileUserImg.setHeight(90)
-        profileUserImg.setWidth(90)
-        profileUserImg.setY(63)
-        profileUserImg.setX(28)
-        profileUserImg.layer.cornerRadius = 90/2
-        profileUserImg.clipsToBounds = true
+        self.profileUserImg.setHeight(self.profileImageSize)
+        self.profileUserImg.setWidth(self.profileImageSize)
+        let profileUserImgY = CGRectGetMinY(profileUserImgBorder.frame)
+        let profileUserImgX = CGRectGetMinX(profileUserImgBorder.frame)
+        self.profileUserImg.setY(profileUserImgY + self.profileImageBorder/2)
+        self.profileUserImg.setX(profileUserImgX + self.profileImageBorder/2)
+        self.profileUserImg.layer.cornerRadius = self.profileImageSize/2
+        self.profileUserImg.clipsToBounds = true
         
         // set up basic user information
-        userName.font = UIFont.getSemiboldFont(20)
-        userName.fillWidth()
-        userName.setHeight(22)
-        userName.setX(30)
-        userName.setY(170)
+        self.userName.font = UIFont.getSemiboldFont(20)
+        self.userName.fillWidth()
+        self.userName.setHeight(20)
+        self.userName.setX(self.leftMargin)
+        let userNameY = CGRectGetMaxY(profileUserImg.frame) + 20
+        self.userName.setY(userNameY)
         
-        userSchool.font = UIFont.normalFont
-        userSchool.fillWidth()
-        userSchool.setHeight(18)
-        userSchool.setX(30)
-        userSchool.setY(200)
+        self.userSchool.font = UIFont.normalFont
+        self.userSchool.fillWidth()
+        self.userSchool.setHeight(12)
+        self.userSchool.setX(self.leftMargin)
+        let userSchoolY = CGRectGetMaxY(userName.frame) + 7
+        self.userSchool.setY(userSchoolY)
         
-        userVolunteerLevel.font = UIFont.normalFont
-        userVolunteerLevel.fillWidth()
-        userVolunteerLevel.setHeight(18)
-        userVolunteerLevel.setX(30)
-        userVolunteerLevel.setY(218)
+        self.userVolunteerLevel.font = UIFont.normalFont
+        self.userVolunteerLevel.fillWidth()
+        self.userVolunteerLevel.setHeight(12)
+        self.userVolunteerLevel.setX(self.leftMargin)
+        let userVolunteerLevelY = CGRectGetMaxY(userSchool.frame) + 7
+        self.userVolunteerLevel.setY(userVolunteerLevelY)
         
         // set up lines
-        topBorder.setHeight(UIConstants.dividerHeight())
-        topBorder.fillWidth()
-        topBorder.backgroundColor = UIColor.borderColor
-        topBorder.setX(0)
-        topBorder.setY(260)
+        self.topBorder.setHeight(UIConstants.dividerHeight())
+        self.topBorder.fillWidth()
+        self.topBorder.backgroundColor = UIColor.borderColor
+        self.topBorder.setX(0)
+        let topBorderY = CGRectGetMaxY(userVolunteerLevel.frame) + 20
+        self.topBorder.setY(topBorderY)
         
-        bottomBorder.setHeight(UIConstants.dividerHeight())
-        bottomBorder.fillWidth()
-        bottomBorder.backgroundColor = UIColor.borderColor
-        bottomBorder.setX(0)
-        bottomBorder.setY(360)
+        self.bottomBorder.setHeight(UIConstants.dividerHeight())
+        self.bottomBorder.fillWidth()
+        self.bottomBorder.backgroundColor = UIColor.borderColor
+        self.bottomBorder.setX(0)
+        self.bottomBorder.setY(topBorderY + self.boxHeight)
         
-        centerBorder.setHeight(100)
-        centerBorder.setWidth(UIConstants.dividerHeight())
-        centerBorder.backgroundColor = UIColor.borderColor
+        self.centerBorder.setHeight(self.boxHeight)
+        self.centerBorder.setWidth(UIConstants.dividerHeight())
+        self.centerBorder.backgroundColor = UIColor.borderColor
         let middle = CGRectGetWidth(topBorder.frame)/2
-        centerBorder.setX(middle)
-        centerBorder.setY(260)
+        self.centerBorder.setX(middle)
+        self.centerBorder.setY(topBorderY)
         
         // set up stats containers
-        userStatusContainer.setX(0)
-        userStatusContainer.setY(261)
-        userStatusContainer.setHeight(99)
-        userStatusContainer.setWidth(middle)
+        self.userStatusContainer.setX(0)
+        self.userStatusContainer.setY(topBorderY+1)
+        self.userStatusContainer.setHeight(self.boxHeight-1)
+        self.userStatusContainer.setWidth(middle)
         
-        userStatusLabel.font = UIFont.normalFont
-        userStatusLabel.numberOfLines = 0
-        userStatusLabel.textAlignment = NSTextAlignment.Center
+        self.userStatusLabel.font = UIFont.normalFont
+        self.userStatusLabel.numberOfLines = 0
+        self.userStatusLabel.textAlignment = NSTextAlignment.Center
         
-        userCommitmentContainer.setX(middle+1)
-        userCommitmentContainer.setY(261)
-        userCommitmentContainer.setWidth(middle)
-        userCommitmentContainer.setHeight(99)
+        self.userCommitmentContainer.setX(middle+1)
+        self.userCommitmentContainer.setY(topBorderY+1)
+        self.userCommitmentContainer.setWidth(middle)
+        self.userCommitmentContainer.setHeight(self.boxHeight-1)
         
-        userCommitmentLabel.font = UIFont.normalFont
-        userCommitmentLabel.numberOfLines = 0
-        userCommitmentLabel.textAlignment = NSTextAlignment.Center
+        self.userCommitmentLabel.font = UIFont.normalFont
+        self.userCommitmentLabel.numberOfLines = 0
+        self.userCommitmentLabel.textAlignment = NSTextAlignment.Center
     }
     
     

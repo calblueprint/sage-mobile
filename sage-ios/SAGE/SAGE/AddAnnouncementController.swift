311f342f9e4a99e66ff9d241c02d713c954fd0c6
Sameera Vemulapalli <sameera.vemulapalli@berkeley.edu>
2015-12-02 12:41:18 -0800

    edit announcement

--- sage-ios/SAGE/SAGE/AddAnnouncementController.swift
+++ sage-ios/SAGE/SAGE/AddAnnouncementController.swift
@@ -35,9 +35,9 @@ class AddAnnouncementController: UIViewController {
         self.navigationController!.pushViewController(tableViewController, animated: true)
     }
     
-    @objc private func completeForm() {
+    func completeForm() {
         if self.addAnnouncementView.isValid() {
-            let finalAnnouncement = self.addAnnouncementView.exportToAnnouncement()
+            let finalAnnouncement = self.exportToAnnouncement()
             AdminOperations.createAnnouncement(finalAnnouncement, completion: { (announcement) -> Void in
                 self.navigationController!.popViewControllerAnimated(true)
                 }) { (errorMessage) -> Void in
@@ -56,6 +56,12 @@ class AddAnnouncementController: UIViewController {
             alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
             self.presentViewController(alertController, animated: true, completion: nil)
         }
+    }
+    
+    private func exportToAnnouncement() -> Announcement {
+        let addView = self.view as! AddAnnouncementView
+        let announcement = Announcement(sender: LoginOperations.getUser(), title: addView.title.textField.text, text: addView.commentField.textView.text, timeCreated: NSDate(timeIntervalSinceNow: 0), school: self.school)
+        return announcement
     }
     
     func didSelectSchool(school: School) {

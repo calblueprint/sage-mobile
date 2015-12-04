71bb4ec1d53fd1257d69ea127ef1fe3721f4e2b6
Kelsey Lam <kelseylam@berkeley.edu>
2015-12-04 15:22:46 -0800

    fixed announcementslistactivity request call

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/announcements/AnnouncementsListActivity.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/announcements/AnnouncementsListActivity.java
@@ -2,27 +2,17 @@ package blueprint.com.sage.announcements;
 
 import android.os.Bundle;
 
-import com.android.volley.Response;
-
-import java.util.ArrayList;
-
 import blueprint.com.sage.R;
-import blueprint.com.sage.events.AnnouncementsListEvent;
-import blueprint.com.sage.models.APIError;
-import blueprint.com.sage.models.Announcement;
 import blueprint.com.sage.network.Requests;
-import blueprint.com.sage.network.announcements.AnnouncementsListRequest;
 import blueprint.com.sage.shared.activities.NavigationAbstractActivity;
-import blueprint.com.sage.utility.network.NetworkManager;
 import blueprint.com.sage.utility.view.FragUtils;
-import de.greenrobot.event.EventBus;
 
 /**
  * Created by kelseylam on 10/24/15.
  */
 public class AnnouncementsListActivity extends NavigationAbstractActivity {
 
-    private ArrayList<Announcement> mAnnouncementsList;
+//   private ArrayList<Announcement> mAnnouncementsList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,27 +23,27 @@ public class AnnouncementsListActivity extends NavigationAbstractActivity {
         FragUtils.replace(R.id.container, AnnouncementsListFragment.newInstance(), this);
     }
 
-    public void announcementsListRequest() {
-        NetworkManager networkManager = NetworkManager.getInstance(this);
-        AnnouncementsListRequest announcementsRequest = new AnnouncementsListRequest(this, null, new Response.Listener<ArrayList<Announcement>>() {
-            @Override
-            public void onResponse(ArrayList<Announcement> announcementsArrayList) {
-                mAnnouncementsList = announcementsArrayList;
-                EventBus.getDefault().post(new AnnouncementsListEvent());
-            }
-        }, new Response.Listener<APIError>() {
-            @Override
-            public void onResponse(APIError apiError) {
-            }
-        });
-        networkManager.getRequestQueue().add(announcementsRequest);
-    }
+//    public void announcementsListRequest() {
+//        NetworkManager networkManager = NetworkManager.getInstance(this);
+//        AnnouncementsListRequest announcementsRequest = new AnnouncementsListRequest(this, null, new Response.Listener<ArrayList<Announcement>>() {
+//            @Override
+//            public void onResponse(ArrayList<Announcement> announcementsArrayList) {
+//                mAnnouncementsList = announcementsArrayList;
+//                EventBus.getDefault().post(new AnnouncementsListEvent());
+//            }
+//        }, new Response.Listener<APIError>() {
+//            @Override
+//            public void onResponse(APIError apiError) {
+//            }
+//        });
+//        networkManager.getRequestQueue().add(announcementsRequest);
+//    }
 
-    public void setmAnnouncementsList(ArrayList<Announcement> announcementsList) {
-        mAnnouncementsList = announcementsList;
-    }
-
-    public ArrayList<Announcement> getmAnnouncementsList() {
-        return mAnnouncementsList;
-    }
+//    public void setmAnnouncementsList(ArrayList<Announcement> announcementsList) {
+//        mAnnouncementsList = announcementsList;
+//    }
+//
+//    public ArrayList<Announcement> getmAnnouncementsList() {
+//        return mAnnouncementsList;
+//    }
 }

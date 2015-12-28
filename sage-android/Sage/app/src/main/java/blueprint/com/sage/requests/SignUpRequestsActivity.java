691f108168a1415bac6a6129a3312a6f82e02215
Charles Xue <charlesx2013@gmail.com>
2015-12-28 02:25:14 -0800

    refactor requests tab

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/requests/SignUpRequestsActivity.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/requests/SignUpRequestsActivity.java
@@ -1,22 +1,24 @@
-package blueprint.com.sage.browse;
+package blueprint.com.sage.requests;
 
 import android.os.Bundle;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import blueprint.com.sage.R;
-import blueprint.com.sage.browse.fragments.UserListFragment;
 import blueprint.com.sage.models.User;
 import blueprint.com.sage.network.Requests;
+import blueprint.com.sage.requests.fragments.VerifyUsersListFragment;
 import blueprint.com.sage.shared.activities.BackAbstractActivity;
 import blueprint.com.sage.shared.interfaces.UsersInterface;
 import blueprint.com.sage.utility.view.FragUtils;
 
 /**
  * Created by charlesx on 12/28/15.
+ * Shows a list of sign up requests
  */
-public class BrowseMentorsActivity extends BackAbstractActivity implements UsersInterface {
+public class SignUpRequestsActivity extends BackAbstractActivity implements UsersInterface {
     private List<User> mUsers;
 
     @Override
@@ -26,13 +28,17 @@ public class BrowseMentorsActivity extends BackAbstractActivity implements Users
         mUsers = new ArrayList<>();
 
         getUsersListRequest();
-        FragUtils.replace(R.id.container, UserListFragment.newInstance(), this);
-    }
-
-    public void getUsersListRequest() {
-        Requests.Users.with(this).makeListRequest(null);
+        FragUtils.replace(R.id.container, VerifyUsersListFragment.newInstance(), this);
     }
 
     public void setUsers(List<User> users) { mUsers = users; }
     public List<User> getUsers() { return mUsers; }
+
+    public void getUsersListRequest() {
+        HashMap<String, String> queryParams = new HashMap<>();
+        queryParams.put("verified", "false");
+
+        Requests.Users.with(this).makeListRequest(queryParams);
+
+    }
 }

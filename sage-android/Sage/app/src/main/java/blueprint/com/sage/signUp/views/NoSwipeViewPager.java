f42ab23cf7fe51bfbd7c2f81b31f10b2fa8f5d3c
Charles Xue <charlesx2013@gmail.com>
2015-12-29 18:58:05 -0800

    resetting view pager

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/signUp/views/NoSwipeViewPager.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/signUp/views/NoSwipeViewPager.java
@@ -3,6 +3,7 @@ package blueprint.com.sage.signUp.views;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 /**
  * Created by charlesx on 10/24/15.
@@ -13,9 +14,9 @@ public class NoSwipeViewPager extends ViewPager {
 
     public NoSwipeViewPager(Context context, AttributeSet attrSet) { super(context, attrSet); }
 
-//    @Override
-//    public boolean onInterceptTouchEvent(MotionEvent event) { return false; }
-//
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) { return false; }
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) { return false; }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) { return false; }
 }

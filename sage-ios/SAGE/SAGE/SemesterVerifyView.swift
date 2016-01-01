35c711f375673e07f34373bb20e103c2c8f31d0c
Andrew Millman <andrewmillman35@gmail.com>
2016-01-01 04:13:10 -0500

    Configure views

--- sage-ios/SAGE/SAGE/SemesterVerifyView.swift
+++ sage-ios/SAGE/SAGE/SemesterVerifyView.swift
@@ -10,4 +10,47 @@ import UIKit
 
 class SemesterVerifyView: UIView {
 
+    private let firstButton = UIView()
+    private let secondButton = UIView()
+    private let buttonSize: CGFloat = 50.0
+    private let buttonMargin: CGFloat = 50.0
+
+    //
+    // MARK: - Initialization and Setup
+    //
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.whiteColor()
+        self.setupSubviews()
+        self.setupActions()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    private func setupSubviews() {
+        self.firstButton.backgroundColor = UIColor.lightRedColor
+        self.firstButton.setSize(width: self.buttonSize, height: self.buttonSize)
+
+        self.secondButton.backgroundColor = UIColor.lightBlueColor
+        self.secondButton.setSize(width: self.buttonSize, height: self.buttonSize)
+    }
+
+    private func setupActions() {
+
+    }
+
+    //
+    // MARK: - Layout
+    //
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        self.firstButton.setX(self.buttonMargin)
+        self.firstButton.alignBottomWithMargin(self.buttonMargin)
+
+        self.secondButton.alignRightWithMargin(self.buttonMargin)
+        self.secondButton.alignBottomWithMargin(self.buttonMargin)
+    }
 }

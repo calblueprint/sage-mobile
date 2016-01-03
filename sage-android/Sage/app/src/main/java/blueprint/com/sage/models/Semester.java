d47e40790955d04a57f20ed23c053c1c8fef4086
Charles Xue <charlesx2013@gmail.com>
2016-01-03 01:04:35 -0800

    added semester model

---
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/models/Semester.java
@@ -0,0 +1,15 @@
+package blueprint.com.sage.models;
+
+import java.util.Date;
+
+import lombok.Data;
+
+/**
+ * Created by charlesx on 1/3/16.
+ * The semester model
+ */
+public @Data class Semester {
+    private int id;
+    private Date start;
+    private Date finish;
+}

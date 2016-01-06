5d9942c7fda18f111de89a5895a5d520a68e4255
Charles Xue <charlesx2013@gmail.com>
2016-01-06 14:52:37 -0800

    removing some old code

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/utility/network/NetworkManager.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/utility/network/NetworkManager.java
@@ -1,25 +1,12 @@
 package blueprint.com.sage.utility.network;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v4.util.LruCache;
-import android.util.Log;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.ImageRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 
 /*
 Singleton Request Handler to interface with Network.
@@ -30,7 +17,6 @@ public class NetworkManager {
      */
     private static NetworkManager mInstance;
     private RequestQueue mRequestQueue;
-    private ImageLoader mImageLoader;
     private static Context mContext;
 
     private static ObjectMapper mObjectMapper;
@@ -44,25 +30,6 @@ public class NetworkManager {
         mContext = context;
         mRequestQueue = Volley.newRequestQueue(mContext);
         getObjectMapper();
-
-        mImageLoader = new ImageLoader(mRequestQueue,
-                new ImageLoader.ImageCache() {
-                    private final LruCache<String, Bitmap>
-                            cache = new LruCache<String, Bitmap>(20);
-
-                    @Override
-                    public Bitmap getBitmap(String url) {
-                        return cache.get(url);
-                    }
-
-                    @Override
-                    public void putBitmap(String url, Bitmap bitmap) {
-                        ByteArrayOutputStream out = new ByteArrayOutputStream();
-                        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, out);
-                        bitmap = BitmapFactory.decodeStream(new ByteArrayInputStream(out.toByteArray()));
-                        cache.put(url, bitmap);
-                    }
-                });
     }
 
     /**
@@ -79,51 +46,6 @@ public class NetworkManager {
         return mInstance;
     }
 
-    /**
-     * Returns an Image Loader for populating Views with images fetched from a URL.
-     *
-     * @return
-     */
-    public ImageLoader getImageLoader() {
-        return mImageLoader;
-    }
-
-    public void imageRequest(String url, final Response.Listener<Bitmap> listener) {
-        ImageRequest request = new ImageRequest(url,
-                new Response.Listener<Bitmap>() {
-                    @Override
-                    public void onResponse(Bitmap bitmap) {
-                        listener.onResponse(bitmap);
-                    }
-                }, 0, 0, null,
-                new Response.ErrorListener() {
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("Volley Error", error.toString());
-                    }
-                });
-        mRequestQueue.add(request);
-    }
-
-    public String stringRequest(String url) {
-        final String[] Stringresponse = new String[1];
-
-        StringRequest request = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.e("INTERNET RESPONSE", "Response is: " + response.substring(0, 500));
-                        Stringresponse[0] = response;
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("INTERNET RESPONSE", "IT DIDN't WORK!");
-            }
-        });
-        mRequestQueue.add(request);
-        return Stringresponse[0];
-    }
-
     // Request Queue
     public RequestQueue getRequestQueue() { return mRequestQueue; }
 
@@ -133,10 +55,6 @@ public class NetworkManager {
             mObjectMapper = createObjectMapper();
         }
         return mObjectMapper;
-    }
-
-    public void setObjectMapper(ObjectMapper objectMapper) {
-        mObjectMapper = objectMapper;
     }
 
     public ObjectMapper createObjectMapper() {

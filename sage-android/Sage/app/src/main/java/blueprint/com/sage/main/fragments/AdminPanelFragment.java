7b390328badc46ce3a665b4a192ee7194cf7a0e5
Charles Xue <charlesx2013@gmail.com>
2016-01-06 21:09:17 -0800

    adding visibility for refreshing the semester

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/main/fragments/AdminPanelFragment.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/main/fragments/AdminPanelFragment.java
@@ -17,7 +17,9 @@ import blueprint.com.sage.models.Semester;
 import blueprint.com.sage.network.Requests;
 import blueprint.com.sage.requests.CheckInRequestsActivity;
 import blueprint.com.sage.requests.SignUpRequestsActivity;
+import blueprint.com.sage.semester.CreateSemesterActivity;
 import blueprint.com.sage.utility.view.FragUtils;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import de.greenrobot.event.EventBus;
@@ -26,6 +28,9 @@ import de.greenrobot.event.EventBus;
  * Created by charlesx on 12/24/15.
  */
 public class AdminPanelFragment extends Fragment {
+
+    @Bind(R.id.admin_settings_start_semester) View mStartSemester;
+    @Bind(R.id.admin_settings_end_semester) View mEndSemester;
 
     private List<Semester> mSemesters;
 
@@ -81,8 +86,18 @@ public class AdminPanelFragment extends Fragment {
         FragUtils.startActivityBackStack(getActivity(), SignUpRequestsActivity.class);
     }
 
-    @OnClick(R.id.admin_settings_semester)
-    public void onSettingsSemester(View view) {
+    @OnClick(R.id.admin_settings_start_semester)
+    public void onStartSemester(View view) {
+        FragUtils.startActivityBackStack(getActivity(), CreateSemesterActivity.class);
+    }
+
+    @OnClick(R.id.admin_settings_end_semester)
+    public void onEndSemester(View view) {
+        // TODO: implement after millman gets his designs in
+    }
+
+    @OnClick(R.id.admin_settings_browse_semesters)
+    public void onBrowseSemesters(View view) {
         // TODO: implement after millman gets his designs in
     }
 
@@ -92,8 +107,16 @@ public class AdminPanelFragment extends Fragment {
     }
 
     private void toggleSemester() {
-        if (mSemesters == null) {
+        mStartSemester.setVisibility(View.GONE);
+        mEndSemester.setVisibility(View.GONE);
 
+        if (mSemesters == null)
+            return;
+        
+        if (mSemesters.size() == 0) {
+            mStartSemester.setVisibility(View.VISIBLE);
+        } else if (mSemesters.size() == 1) {
+            mStartSemester.setVisibility(View.VISIBLE);
         }
     }
 }

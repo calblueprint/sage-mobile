431b940c1903332966a18faec28c5caa33942974
Charles Xue <charlesx2013@gmail.com>
2016-01-07 01:37:12 -0800

    finish semester skeleton

---
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/semester/fragments/FinishSemesterFragment.java
@@ -0,0 +1,32 @@
+package blueprint.com.sage.semester.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import blueprint.com.sage.R;
+import butterknife.ButterKnife;
+
+/**
+ * Created by charlesx on 1/7/16.
+ * Finish a semester
+ */
+public class FinishSemesterFragment extends Fragment {
+
+    public static FinishSemesterFragment newInstance() { new FinishSemesterFragment(); }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        super.onCreateView(inflater, parent, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_finish_semester, parent, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+}

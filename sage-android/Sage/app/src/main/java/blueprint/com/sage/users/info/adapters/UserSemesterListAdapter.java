1410d4c38b4fd853d003b322a84930bd250f18f0
Charles Xue <charlesx2013@gmail.com>
2016-01-16 03:27:41 -0800

    add user semester list fragment skeleton

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/users/info/adapters/UserSemesterListAdapter.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/users/info/adapters/UserSemesterListAdapter.java
@@ -5,19 +5,28 @@ import android.view.View;
 
 import java.util.List;
 
+import blueprint.com.sage.R;
 import blueprint.com.sage.models.Semester;
+import blueprint.com.sage.models.User;
 import blueprint.com.sage.shared.adapters.models.AbstractSemesterListAdapter;
+import blueprint.com.sage.users.info.fragments.UserSemesterFragment;
 import blueprint.com.sage.utility.view.FragUtils;
 
 /**
  * Created by charlesx on 1/15/16.
  */
 public class UserSemesterListAdapter extends AbstractSemesterListAdapter {
-    public UserSemesterListAdapter(FragmentActivity activity, List<Semester> semesters) {
+
+    private User mUser;
+
+    public UserSemesterListAdapter(FragmentActivity activity, List<Semester> semesters, User user) {
         super(activity, semesters);
+        mUser = user;
     }
 
     public void onItemClicked(View view, Semester semester) {
-        FragUtils.replaceBackStack(view, );
+        FragUtils.replaceBackStack(R.id.container,
+                UserSemesterFragment.newInstance(mUser, semester),
+                mActivity);
     }
 }
--- sage-android/Sage/app/src/main/java/blueprint/com/sage/users/info/fragments/UserSemesterFragment.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/users/info/fragments/UserSemesterFragment.java
@@ -1,10 +1,43 @@
 package blueprint.com.sage.users.info.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import blueprint.com.sage.R;
+import blueprint.com.sage.models.Semester;
+import blueprint.com.sage.models.User;
+import butterknife.ButterKnife;
 
 /**
  * Created by charlesx on 1/15/16.
  */
 public class UserSemesterFragment extends Fragment {
+    private User mUser;
+    private Semester mSemester;
 
+    public static UserSemesterFragment newInstance(User user, Semester semester) {
+        UserSemesterFragment fragment = new UserSemesterFragment();
+        fragment.setUser(user);
+        fragment.setSemester(semester);
+        return fragment;
+    }
+
+    public void setUser(User user) { mUser = user; }
+    public void setSemester(Semester semester) { mSemester = semester; }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        super.onCreateView(inflater, parent, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_user_semester, parent, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
 }

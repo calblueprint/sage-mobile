8849f3e99dbc33c5bee6fe664d597048d20cf41a
Charles Xue <charlesx2013@gmail.com>
2016-01-30 03:55:11 -0800

    added fragment to join semester activity and added activity to manifest

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/joinSemester/JoinSemesterActivity.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/joinSemester/JoinSemesterActivity.java
@@ -1,7 +1,20 @@
 package blueprint.com.sage.joinSemester;
 
+import android.os.Bundle;
+
+import blueprint.com.sage.R;
+import blueprint.com.sage.joinSemester.fragments.JoinSemesterFragment;
+import blueprint.com.sage.shared.activities.BackAbstractActivity;
+import blueprint.com.sage.utility.view.FragUtils;
+
 /**
  * Created by charlesx on 1/30/16.
  */
-public class JoinSemesterActivity {
+public class JoinSemesterActivity extends BackAbstractActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FragUtils.replace(R.id.main_container, JoinSemesterFragment.newInstance(), this);
+    }
 }

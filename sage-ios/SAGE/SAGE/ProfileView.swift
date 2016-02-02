5b48cccb1f0a0409a442808e577718d51d4a6b56
Andrew Millman <andrewmillman35@gmail.com>
2016-02-01 16:55:09 -0800

    Make profile view display required hours

--- sage-ios/SAGE/SAGE/ProfileView.swift
+++ sage-ios/SAGE/SAGE/ProfileView.swift
@@ -123,7 +123,8 @@ class ProfileView: UIView {
 
         if name == self.userStatusString {
             string.addAttributes(boldString, range: NSRange(location: 0, length: length))
-            string.addAttribute(NSForegroundColorAttributeName, value: UIColor.secondaryTextColor, range: NSRange(location: length+6, length: 6))
+            let remainderLocation = string.length - 5
+            string.addAttribute(NSForegroundColorAttributeName, value: UIColor.secondaryTextColor, range: NSRange(location: remainderLocation, length: 5))
         }
         
         if name == self.userCommitmentString {
@@ -143,7 +144,7 @@ class ProfileView: UIView {
         if let semesterSummary = user.semesterSummary {
             self.userStatusLabel.font = UIFont.normalFont
             let hoursCompletedString = String(semesterSummary.getTotalHours())
-            let userStatusString = NSMutableAttributedString(string: hoursCompletedString + " / 60 \nhours") // ask charles about the 60
+            let userStatusString = NSMutableAttributedString(string: hoursCompletedString + " / " + String(semesterSummary.hoursRequired) + " \nhours")
             styleAttributedString(self.userStatusString, string: userStatusString, length: hoursCompletedString.characters.count)
             self.userStatusLabel.attributedText = userStatusString
         } else {

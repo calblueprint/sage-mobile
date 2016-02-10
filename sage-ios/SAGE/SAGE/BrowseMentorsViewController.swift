a336a37f5a271302feb4f8da96d06d4e56b44ac4
Andrew Millman <andrewmillman35@gmail.com>
2016-02-09 17:24:56 -0800

    Fix compatability issues in iOS 8

--- sage-ios/SAGE/SAGE/BrowseMentorsViewController.swift
+++ sage-ios/SAGE/SAGE/BrowseMentorsViewController.swift
@@ -18,6 +18,10 @@ class BrowseMentorsViewController: UITableViewController {
         super.init(style: .Plain)
     }
 
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- sage-ios/SAGE/SAGE/JoinSemesterViewController.swift
+++ sage-ios/SAGE/SAGE/JoinSemesterViewController.swift
@@ -25,6 +25,10 @@ class JoinSemesterViewController: UIViewController {
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "semesterEnded:", name: NotificationConstants.endSemesterKey, object: nil)
     }
     
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }

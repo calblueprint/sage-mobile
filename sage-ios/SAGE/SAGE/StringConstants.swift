ea871ee8696fde4fbd8be1ac30c9319cbc8d03b0
Andrew Millman <andrewmillman35@gmail.com>
2016-02-11 21:43:43 -0800

    Set staging server as server when in debug mode

--- sage-ios/SAGE/SAGE/StringConstants.swift
+++ sage-ios/SAGE/SAGE/StringConstants.swift
@@ -14,9 +14,12 @@ struct StringConstants {
     static let displayDateFormat = "yyyy/MM/dd hh:mm a"
     static let JSONdateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
     
-    static let kEndpointBaseURL = "http://sage-rails.herokuapp.com/api/v1"
-    //static let kEndpointBaseURL = "http://sage-rails-staging.herokuapp.com/api/v1"
-    //static let kEndpointBaseURL = "http://localhost:3000/api/v1"
+    #if DEBUG
+        static let kEndpointBaseURL = "http://sage-rails-staging.herokuapp.com/api/v1"
+    #else
+        static let kEndpointBaseURL = "http://sage-rails.herokuapp.com/api/v1"
+    #endif
+
     static let kEndpointCreateUser = kEndpointBaseURL + "/users"
     static let kEndpointLogin = kEndpointBaseURL + "/users/sign_in"
     static let kEndpointLogout = kEndpointBaseURL + "/users/sign_out"

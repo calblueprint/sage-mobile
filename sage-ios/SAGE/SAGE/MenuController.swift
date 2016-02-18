b44d1db133e381ac96dc4b19cec4c23beed438c9
Andrew Millman <andrewmillman35@gmail.com>
2016-02-17 16:59:50 -0800

    Create menu view and view controller

---
+++ sage-ios/SAGE/SAGE/MenuController.swift
@@ -0,0 +1,38 @@
+//
+//  MenuController.swift
+//  SAGE
+//
+//  Created by Andrew Millman on 2/17/16.
+//  Copyright © 2016 Cal Blueprint. All rights reserved.
+//
+
+import Foundation
+
+class MenuController: UIViewController {
+    
+    var menuView = MenuView()
+    
+    //
+    // MARK: - Initialization
+    //
+    required init(title: String?) {
+        super.init(nibName: nil, bundle: nil)
+        self.title = title
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //
+    // MARK: - ViewController LifeCycle
+    //
+    override func loadView() {
+        self.view = self.menuView
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.menuView.appear()
+    }
+}

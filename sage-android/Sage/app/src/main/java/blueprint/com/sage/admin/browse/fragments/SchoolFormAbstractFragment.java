8cd3a2d418ac8da83a52bb553eacde03443df96d
Kelsey Lam <kelseylam@berkeley.edu>
2016-02-24 17:27:58 -0800

    added radius slider/circle to schoolabstractfragment and schoolfragment. created scrollmapview class and updated maputils

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/admin/browse/fragments/SchoolFormAbstractFragment.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/admin/browse/fragments/SchoolFormAbstractFragment.java
@@ -22,7 +22,6 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -74,6 +73,8 @@ public abstract class SchoolFormAbstractFragment extends Fragment
     private BaseInterface mBaseInterface;
     private PlacePredictionAdapter mPlaceAdapter;
     private FormValidator mValidator;
+    protected MenuItem mItem;
+
     private LatLng mBounds;
 
     private final int MAX_RADIUS = 1000;

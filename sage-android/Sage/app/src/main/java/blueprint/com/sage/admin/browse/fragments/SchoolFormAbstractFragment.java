8e7b9a6b592557c507d2620c3376bba3106f83df
Kelsey Lam <kelseylam@berkeley.edu>
2016-02-24 18:19:51 -0800

    fixed spaces in colors.xml and radius.setText in schoolform

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/admin/browse/fragments/SchoolFormAbstractFragment.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/admin/browse/fragments/SchoolFormAbstractFragment.java
@@ -206,6 +206,7 @@ public abstract class SchoolFormAbstractFragment extends Fragment
         }
 
         mRadiusInt.setText(initialRadius + " m");
+        mMapView.setRadius(initialRadius);
 
         mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
             @Override

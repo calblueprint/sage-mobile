5e222cc1b90d4579ed993bb60e291adf3f958ad3
Charles Xue <charlesx2013@gmail.com>
2016-02-26 04:19:57 -0800

    Added animation for loading circles
    Test Plan:
    
    Reviewers:
    
    Subscribers: samz, kelseylam, baerica, amillman
    
    Projects: #Sage-Mobile

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/utility/view/AnimationUtils.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/utility/view/AnimationUtils.java
@@ -1,19 +1,14 @@
 package blueprint.com.sage.utility.view;
 
-import android.view.animation.Interpolator;
-import android.view.animation.TranslateAnimation;
+import android.animation.ObjectAnimator;
+import android.view.View;
 
 /**
  * Created by charlesx on 2/25/16.
  */
 public class AnimationUtils {
 
-    public static final int Y_DURATION = 500;
-
-    public static TranslateAnimation getYTranslationAnimator(int start, int end, Interpolator interpolator) {
-        TranslateAnimation animation = new TranslateAnimation(0, 0, start, end);
-        animation.setDuration(Y_DURATION);
-        animation.setInterpolator(interpolator);
-        return animation;
+    public static ObjectAnimator getYTranslationAnimator(View view, int start, int end) {
+        return ObjectAnimator.ofFloat(view, "y", start, end);
     }
 }

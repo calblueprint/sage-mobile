d44777ebd75b407ea11c74d53005af4adb06d2f2
Charles Xue <charlesx2013@gmail.com>
2016-02-26 02:16:56 -0800

    Summary: Adding about fragment to test view and added LoadingView
    Test Plan:
    
    Reviewers:
    
    Subscribers: samz, kelseylam, baerica, amillman
    
    Projects: #Sage-Mobile

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/utility/view/AnimationUtils.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/utility/view/AnimationUtils.java
@@ -1,8 +1,5 @@
 package blueprint.com.sage.utility.view;
 
-import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.Animation;
 import android.view.animation.Interpolator;
 import android.view.animation.TranslateAnimation;
 
@@ -12,26 +9,6 @@ import android.view.animation.TranslateAnimation;
 public class AnimationUtils {
 
     public static final int Y_DURATION = 500;
-
-    public static TranslateAnimation getUpDownAnimator(final View view, int start, int end) {
-        TranslateAnimation toTop = getYTranslationAnimator(start, end, new AccelerateDecelerateInterpolator());
-        final TranslateAnimation toBottom = getYTranslationAnimator(end, start, new AccelerateDecelerateInterpolator());
-        toTop.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                view.setAnimation(toBottom);
-                toBottom.start();
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {}
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {}
-        });
-
-        return toTop;
-    }
 
     public static TranslateAnimation getYTranslationAnimator(int start, int end, Interpolator interpolator) {
         TranslateAnimation animation = new TranslateAnimation(0, 0, start, end);

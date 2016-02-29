4a6e3d40b3377a8dc808298390aa9aa85ae00caa
Andrew Millman <andrewmillman35@gmail.com>
2016-02-28 22:36:10 -0800

    Add filtering option functionality in code for announcements

--- sage-ios/SAGE/SAGE/AnnouncementsOperations.swift
+++ sage-ios/SAGE/SAGE/AnnouncementsOperations.swift
@@ -11,20 +11,21 @@ import AFNetworking
 
 class AnnouncementsOperations {
     
-    static func loadAnnouncements(completion: (([Announcement]) -> Void), failure:((String) -> Void)) {
-        var params: [String: String]
-        if LoginOperations.getUser()!.role == .Admin || LoginOperations.getUser()!.role == .President {
-            params = [
-                NetworkingConstants.kSortAttr: CheckinConstants.kTimeCreated,
-                NetworkingConstants.kSortOrder: NetworkingConstants.kDescending
-            ]
-        } else {
-            let schoolID = LoginOperations.getUser()!.school!.id
-            params = [
-                NetworkingConstants.kSortAttr: CheckinConstants.kTimeCreated,
-                NetworkingConstants.kSortOrder: NetworkingConstants.kDescending,
-                AnnouncementConstants.kDefault: String(schoolID)
-            ]
+    static func loadAnnouncements(filter filter: [String: String]? = nil, completion: (([Announcement]) -> Void), failure:((String) -> Void)) {
+        var params = [
+            NetworkingConstants.kSortAttr: CheckinConstants.kTimeCreated,
+            NetworkingConstants.kSortOrder: NetworkingConstants.kDescending
+        ]
+        if !(LoginOperations.getUser()!.role == .Admin || LoginOperations.getUser()!.role == .President) {
+            if filter == nil {
+                let schoolID = LoginOperations.getUser()!.school!.id
+                params[AnnouncementConstants.kDefault] = String(schoolID)
+            }
+        }
+        if let filter = filter as [String: String]! {
+            for (key, value) in filter {
+                params[key] = value
+            }
         }
 
         BaseOperation.manager().GET(StringConstants.kEndpointAnnouncements, parameters: params, success: { (operation, data) -> Void in

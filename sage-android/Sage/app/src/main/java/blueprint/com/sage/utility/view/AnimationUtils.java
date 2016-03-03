addcc53f24229c5a8e67d06c9d23fe786e4de506
Charles Xue <charlesx2013@gmail.com>
2016-03-03 03:35:22 -0800

    Summary: Added filtering view and animation
    Test Plan:
    
    Reviewers:
    
    Subscribers: samz, kelseylam, baerica, amillman
    
    Projects: #Sage-Mobile

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/utility/view/AnimationUtils.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/utility/view/AnimationUtils.java
@@ -1,7 +1,12 @@
 package blueprint.com.sage.utility.view;
 
 import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.view.View;
+import android.view.animation.Animation;
+
+import blueprint.com.sage.R;
+import blueprint.com.sage.shared.interpolators.CustomBounceInterpolator;
 
 /**
  * Created by charlesx on 2/25/16.
@@ -10,5 +15,11 @@ public class AnimationUtils {
 
     public static ObjectAnimator getYTranslationAnimator(View view, int start, int end) {
         return ObjectAnimator.ofFloat(view, "y", start, end);
+    }
+
+    public static Animation getBounceSlideUpAnimator(Context context) {
+        Animation animation = android.view.animation.AnimationUtils.loadAnimation(context, R.anim.slide_from_top);
+        animation.setInterpolator(new CustomBounceInterpolator());
+        return animation;
     }
 }

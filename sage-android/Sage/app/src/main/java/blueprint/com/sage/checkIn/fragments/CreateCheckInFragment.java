fca65617dd1dd5a51da4d2aeea466fb8d92a74ee
Charles Xue <charlesx2013@gmail.com>
2016-03-02 22:01:10 -0800

    Summary: Rename method
    Test Plan:
    
    Reviewers:
    
    Subscribers: samz, kelseylam, baerica, amillman
    
    Projects: #Sage-Mobile

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/checkIn/fragments/CreateCheckInFragment.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/checkIn/fragments/CreateCheckInFragment.java
@@ -114,7 +114,7 @@ public class CreateCheckInFragment extends Fragment implements FormValidation, D
         mItem = item;
         switch (item.getItemId()) {
             case R.id.menu_save:
-                validateAndSubmitschoolRequest();
+                validateAndSubmitRequest();
             default:
                 return super.onOptionsItemSelected(item);
         }

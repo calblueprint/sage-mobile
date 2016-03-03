bac7dc09b030615aff61e64c4c1eb3da70a98ca7
Kelsey Lam <kelseylam@berkeley.edu>
2016-03-02 22:29:52 -0800

    reformatted semester list item
    moved present string from User.java to DateUtils, and also added ABBREV_YEAR_FORMAT to DateUtils
    
    moved getdaterange into dateutils
    
    added radius slider/circle to schoolabstractfragment and schoolfragment. created scrollmapview class and updated maputils
    
    Summary:
    Small error
    
    Test Plan:
    
    Reviewers:
    
    Subscribers: samz, kelseylam, baerica, amillman
    
    Projects: #Sage-Mobile
    
    added radius slider/circle to schoolabstractfragment and schoolfragment. created scrollmapview class and updated maputils
    
    reformatted semester list item
    
    moved getdaterange into dateutils
    
    added radius slider/circle to schoolabstractfragment and schoolfragment. created scrollmapview class and updated maputils
    
    Summary:
    Remove loading screen if there arent many more models
    
    Test Plan:
    
    Reviewers:
    
    Subscribers: samz, kelseylam, baerica, amillman
    
    Projects: #Sage-Mobile
    
    Summary:
    Rename method
    
    Test Plan:
    
    Reviewers:
    
    Subscribers: samz, kelseylam, baerica, amillman
    
    Projects: #Sage-Mobile
    
    Summary:
    idea file
    
    Test Plan:
    
    Reviewers:
    
    Subscribers: samz, kelseylam, baerica, amillman
    
    Projects: #Sage-Mobile
    
    Summary:
    Remove default filter, add myschool filter
    
    Test Plan:
    
    Reviewers:
    
    Subscribers: samz, kelseylam, baerica, amillman
    
    Projects: #Sage-Mobile
    
    moved present string from User.java to DateUtils, and also added ABBREV_YEAR_FORMAT to DateUtils
    
    turned hard coded summary string into a resource, made another datetime format string in dateutils, deleted dimens_user_semester
    
    added forpattern to dateutils and deleted getdatetime from checkin.java
    
    fixed spaces in colors.xml and radius.setText in schoolform
    
    reformatted semester list item
    
    added check in summary for user and also modified check in list item
    
    moved present string from User.java to DateUtils, and also added ABBREV_YEAR_FORMAT to DateUtils
    
    turned hard coded summary string into a resource, made another datetime format string in dateutils, deleted dimens_user_semester
    
    added forpattern to dateutils and deleted getdatetime from checkin.java
    
    moved getdaterange into dateutils
    
    added radius slider/circle to schoolabstractfragment and schoolfragment. created scrollmapview class and updated maputils
    
    reformatted semester list item
    
    moved present string from User.java to DateUtils, and also added ABBREV_YEAR_FORMAT to DateUtils
    
    moved getdaterange into dateutils
    
    Summary:
    Adding in success event
    
    Test Plan:
    
    Reviewers:
    
    Subscribers: samz, kelseylam, baerica, amillman
    
    Projects: #Sage-Mobile

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/admin/browse/fragments/SchoolFormAbstractFragment.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/admin/browse/fragments/SchoolFormAbstractFragment.java
@@ -22,7 +22,6 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -78,7 +77,6 @@ public abstract class SchoolFormAbstractFragment extends Fragment
 
     protected MenuItem mItem;
     private LatLng mBounds;
-
     private final int MAX_RADIUS = 1000;
 
     @Override
@@ -211,6 +209,7 @@ public abstract class SchoolFormAbstractFragment extends Fragment
         }
 
         mRadiusInt.setText(initialRadius + " m");
+        mMapView.setRadius(initialRadius);
 
         mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
             @Override

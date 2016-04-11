939c8590c7249dcde04a066b244afdf6bd32a3de
Charles Xue <charlesx2013@gmail.com>
2016-04-11 16:24:40 -0700

    Summary: Adding pop back stack
    Test Plan:
    
    Reviewers:
    
    Subscribers: samz, kelseylam, baerica, amillman
    
    Projects: #Sage-Mobile

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/signin/fragments/ResetPasswordFragment.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/signin/fragments/ResetPasswordFragment.java
@@ -6,16 +6,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.HashMap;
 
 import blueprint.com.sage.R;
+import blueprint.com.sage.events.APIErrorEvent;
+import blueprint.com.sage.events.sessions.ResetPasswordEvent;
 import blueprint.com.sage.network.Requests;
 import blueprint.com.sage.shared.FormValidation;
 import blueprint.com.sage.shared.validators.FormValidator;
+import blueprint.com.sage.shared.views.SimpleLoadingLayout;
+import blueprint.com.sage.utility.view.FragUtils;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import de.greenrobot.event.EventBus;
 
 /**
  * Created by charlesx on 4/9/16.
@@ -23,6 +29,7 @@ import butterknife.OnClick;
 public class ResetPasswordFragment extends Fragment implements FormValidation {
 
     @Bind(R.id.reset_password_email) EditText mResetPasswordEmail;
+    @Bind(R.id.reset_password_button) SimpleLoadingLayout mResetPasswordButton;
 
     private FormValidator mFormValidator;
 
@@ -42,6 +49,18 @@ public class ResetPasswordFragment extends Fragment implements FormValidation {
         return view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
     @OnClick(R.id.reset_password_button)
     public void onResetClick() {
         validateAndSubmitRequest();
@@ -53,11 +72,22 @@ public class ResetPasswordFragment extends Fragment implements FormValidation {
 
         HashMap<String, String> params = new HashMap<>();
         params.put("email", mResetPasswordEmail.getText().toString());
+
         Requests.Sessions.with(getActivity()).makeResetPasswordRequest(params);
+        mResetPasswordButton.startSpinning();
     }
 
     private boolean isValid() {
         return mFormValidator.hasNonBlankField(mResetPasswordEmail, getString(R.string.cannot_be_blank)) &&
                 mFormValidator.hasValidEmail(mResetPasswordEmail);
+    }
+
+    public void onEvent(ResetPasswordEvent event) {
+        Toast.makeText(getActivity(), event.getApiSuccess().getMessage(), Toast.LENGTH_SHORT);
+        FragUtils.popBackStack(this);
+    }
+
+    public void onEvent(APIErrorEvent event) {
+        mResetPasswordButton.stopSpinning();
     }
 }

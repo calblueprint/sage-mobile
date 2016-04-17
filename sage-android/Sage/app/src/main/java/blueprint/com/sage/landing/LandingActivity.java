047dd5d768fe2d2334f121f051801f854be7ac14
Kelsey Lam <kelseylam@berkeley.edu>
2016-04-17 03:01:55 -0700

    tentative changes to landing activity

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/landing/LandingActivity.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/landing/LandingActivity.java
@@ -4,20 +4,30 @@ import blueprint.com.sage.shared.activities.AbstractActivity;
 
 import android.content.Intent;
 import android.content.BroadcastReceiver;
+<<<<<<< HEAD
 import android.content.Context;
+=======
+>>>>>>> tentative changes to landing activity
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+<<<<<<< HEAD
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+=======
+>>>>>>> tentative changes to landing activity
 import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import org.json.JSONObject;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -28,7 +38,6 @@ import blueprint.com.sage.events.SessionEvent;
 import blueprint.com.sage.main.MainActivity;
 import blueprint.com.sage.models.Session;
 import blueprint.com.sage.network.Requests;
-import blueprint.com.sage.notifications.RegistrationIntentService;
 import blueprint.com.sage.shared.activities.AbstractActivity;
 import blueprint.com.sage.signIn.SignInActivity;
 import blueprint.com.sage.signUp.UnverifiedActivity;
@@ -56,6 +65,10 @@ public class LandingActivity extends AbstractActivity {
     private SharedPreferences mPreferences;
     private BroadcastReceiver mRegistrationBroadcastReceiver;
     private boolean mIsReceiverRegistered;
+    private String mRegistrationId;
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "LandingActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,9 +76,13 @@ public class LandingActivity extends AbstractActivity {
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_landing);
 
+<<<<<<< HEAD
         ButterKnife.bind(this);
 
         initializeViews();
+=======
+        mPreferences = getSharedPreferences();
+>>>>>>> tentative changes to landing activity
 
         if (!NetworkUtils.isConnectedToInternet(this)) {
             Toast.makeText(this, "You're not connected to the internet!", Toast.LENGTH_SHORT).show();
@@ -78,11 +95,40 @@ public class LandingActivity extends AbstractActivity {
             return;
         }
 
+        mRegistrationId = mPreferences.getString("registration_id", "none");
+
+        if (getRegistrationId().isEmpty()) {
+            registerInBackground();
+        }
+
         Requests.Users.with(this).makeStateRequest(getUser());
 
         // Register Broadcast Receiver
-        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+//        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+//            @Override
+//            public void onReceive(Context context, Intent intent) {
+////                boolean sentToken = mPreferences
+////                        .getBoolean(getString(R.string.sent_token_to_server), false);
+////                if (sentToken) {
+////                    mInformationTextView.setText(getString(R.string.gcm_send_message));
+////                } else {
+////                    mInformationTextView.setText(getString(R.string.token_error_message));
+////                }
+//            }
+//        };
+//        registerReceiver();
+//
+//        if (checkPlayServices()) {
+//            // Start IntentService to register this application with GCM.
+//            Intent intent = new Intent(this, RegistrationIntentService.class);
+//            startService(intent);
+//        }
+    }
+
+    public void registerInBackground() {
+        new AsyncTask<String, String, String>() {
             @Override
+<<<<<<< HEAD
             public void onReceive(Context context, Intent intent) {
                 boolean sentToken = mPreferences
                         .getBoolean(QuickStartPreferences.SENT_TOKEN_TO_SERVER, false);
@@ -90,16 +136,26 @@ public class LandingActivity extends AbstractActivity {
                     mInformationTextView.setText(getString(R.string.gcm_send_message));
                 } else {
                     mInformationTextView.setText(getString(R.string.token_error_message));
+=======
+            protected String doInBackground(String... params) {
+                String msg = "";
+                JSONObject user = new JSONObject();
+                JSONObject objParams = new JSONObject();
+                try {
+                    if (mGoogleCloudMessaging == null) mGoogleCloudMessaging = GoogleCloudMessaging.getInstance(LandingActivity.this);
+                    objParams.put("registration_id", mGoogleCloudMessaging.register(SENDER_ID));
+                    objParams.put("device_type", 0);
+                    user.put("user", objParams);
+                } catch (Exception ex) {
+                    msg = "Error :" + ex.getMessage();
+>>>>>>> tentative changes to landing activity
                 }
+                return msg;
             }
-        };
-        registerReceiver();
 
-        if (checkPlayServices()) {
-            // Start IntentService to register this application with GCM.
-            Intent intent = new Intent(this, RegistrationIntentService.class);
-            startService(intent);
-        }
+            @Override
+            protected void onPostExecute(String msg) { Log.i("ERROR", msg + "\n"); }
+        }.execute(null, null, null);
     }
 
     @Override
@@ -188,22 +244,44 @@ public class LandingActivity extends AbstractActivity {
     @Override
     protected void onPause() {
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
-        isReceiverRegistered = false;
+        mIsReceiverRegistered = false;
         super.onPause();
     }
 
+    /**
+     * Gets the current registration ID for application on GCM service.
+     * If result is empty, the app needs to register.
+     *
+     * @return registration ID, or empty string if there is no existing
+     *         registration ID.
+     */
+    private String getRegistrationId() {
+        if (mRegistrationId.equals("none")) {
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+
+        // Check if app was updated; if so, it must clear the registration ID
+        // since the existing registration ID is not guaranteed to work with
+        // the new app version.
+//        int currentVersion = Utility.getAppVersion(this);
+//        if (mAppVersion != currentVersion) {
+//            Log.i(TAG, "App version changed.");
+//            mPreferences.edit().putInt("app_version", currentVersion).apply();
+//            return "";
+//        }
+        return mRegistrationId;
+    }
+
     private void registerReceiver(){
-        if(!isReceiverRegistered) {
+
+        if(!mIsReceiverRegistered) {
             LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
-                    new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
-            isReceiverRegistered = true;
+                    new IntentFilter(getString(R.string.registration_complete)));
+            mIsReceiverRegistered = true;
         }
     }
-    /**
-     * Check the device to make sure it has the Google Play Services APK. If
-     * it doesn't, display a dialog that allows users to download the APK from
-     * the Google Play Store or enable it in the device's system settings.
-     */
+
     private boolean checkPlayServices() {
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
         int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);

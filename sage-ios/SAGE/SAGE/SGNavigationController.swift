dab74b52c8adc7929dfedfe43ec228fbf6941176
Sameera Vemulapalli <sameera.vemulapalli@berkeley.edu>
2016-04-20 21:45:30 -0700

    basic successview and sgnaviationcontroller

---
+++ sage-ios/SAGE/SAGE/SGNavigationController.swift
@@ -0,0 +1,21 @@
+//
+//  SGNavigationController.swift
+//  SAGE
+//
+//  Created by Sameera Vemulapalli on 4/20/16.
+//  Copyright © 2016 Cal Blueprint. All rights reserved.
+//
+
+import UIKit
+
+class SGNavigationController: UINavigationController {
+    
+    private var successView: SuccessView?
+
+    func showSuccessAndSetMessage(message: String) {
+        let success = self.successView
+        let successView = super.showSuccess(message, currentSuccess: success, color: UIColor.lightRedColor)
+        self.successView = successView
+    }
+
+}

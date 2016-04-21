2da0ce574fc1747452824b18fc86fea2f4a8a570
Sameera Vemulapalli <sameera.vemulapalli@berkeley.edu>
2016-04-20 21:45:30 -0700

    basic successview and sgnaviationcontroller

---
+++ sage-ios/SAGE/SAGE/SuccessView.swift
@@ -0,0 +1,63 @@
+//
+//  SuccessView.swift
+//  SAGE
+//
+//  Created by Sameera Vemulapalli on 4/20/16.
+//  Copyright © 2016 Cal Blueprint. All rights reserved.
+//
+
+import UIKit
+
+class SuccessView: UIView {
+
+    var message: UILabel = UILabel()
+    var background: UIView = UIView()
+    var centered: Bool = true
+
+    required override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    convenience init(height: CGFloat, messageString: String, color: UIColor = UIColor.whiteColor(), alpha: CGFloat? = 1.0, centered: Bool? = true) {
+        let screenRect = UIScreen.mainScreen().bounds;
+        let screenWidth = screenRect.size.width;
+        let newFrame = CGRectMake(0, 0, screenWidth, height)
+        self.init(frame: newFrame)
+
+        self.addSubview(self.background)
+        self.background.backgroundColor = color
+        self.background.alpha = alpha!
+
+        self.addSubview(self.message)
+        self.message.text = messageString
+        self.message.textColor = UIColor.whiteColor()
+        self.message.font = UIFont.normalFont
+        self.message.lineBreakMode = .ByWordWrapping
+        self.message.numberOfLines = 2
+        self.centered = centered!
+
+        self.userInteractionEnabled = true
+        let successTapRecognizer = UITapGestureRecognizer()
+        successTapRecognizer.addTarget(self, action: "errorMessageTapped")
+        self.addGestureRecognizer(successTapRecognizer)
+    }
+
+    func errorMessageTapped() {
+        self.alpha = 0.0
+    }
+
+    override func layoutSubviews() {
+        self.message.sizeToFit()
+        self.message.centerHorizontally()
+        self.message.centerVertically()
+        if !self.centered {
+            self.message.setY(self.message.frame.origin.y + 10)
+        }
+        self.background.frame = self.frame
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
--- sage-ios/SAGE/SAGE/UIViewController.swift
+++ sage-ios/SAGE/SAGE/UIViewController.swift
@@ -37,6 +37,33 @@ extension UIViewController {
         return errorView
     }
     
+    func showSuccess(message: String, currentSuccess: SuccessView?, color: UIColor = UIColor.whiteColor(), alpha: CGFloat? = 1.0, centered: Bool? = true) -> SuccessView {
+        if currentSuccess != nil {
+            currentSuccess?.removeFromSuperview()
+        }
+        var size: CGFloat
+        if let _ = self.navigationController {
+            size = 44.0
+        } else {
+            size = 64.0
+        }
+        let successView = SuccessView(height: size, messageString: message, color: color, alpha: alpha, centered: centered)
+        self.view.addSubview(successView)
+        self.view.bringSubviewToFront(successView)
+        successView.setX(0)
+        successView.setY(-10)
+
+        UIView.animateWithDuration(UIView.animationTime, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.1, options: UIViewAnimationOptions.CurveLinear, animations: { () -> Void in
+            successView.setY(0)
+            }) { (bool) -> Void in
+                UIView.animateWithDuration(UIView.animationTime, delay: 3, options: .CurveLinear, animations: { () -> Void in
+                    successView.alpha = 0.0
+                    successView.setY(-1 * size)
+                    }, completion: nil)
+        }
+        return successView
+    }
+
     func showAlertControllerError(errorMessage: String) {
         let alertController = UIAlertController(
             title: "Failure",

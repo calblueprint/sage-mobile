e0b34019f184ab3e946a989e32c04b832c67ba46
Andrew Millman <andrewmillman35@gmail.com>
2016-04-27 21:25:01 -0700

    Correct the y-positioning of the success view

--- sage-ios/SAGE/SAGE/UINavigationController.swift
+++ sage-ios/SAGE/SAGE/UINavigationController.swift
@@ -20,14 +20,14 @@ extension UINavigationController {
         self.view.insertSubview(successView, belowSubview: self.navigationBar)
         successView.layoutSubviews()
         successView.setX(0)
-        successView.setY(-10)
+        successView.setY(-10 + UIConstants.navbarHeight)
 
         UIView.animateWithDuration(UIView.animationTime, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.1, options: UIViewAnimationOptions.CurveLinear, animations: { () -> Void in
             successView.setY(UIConstants.navbarHeight)
             }) { (bool) -> Void in
                 UIView.animateWithDuration(UIView.animationTime, delay: 3, options: .CurveLinear, animations: { () -> Void in
                     successView.alpha = 0.0
-                    successView.setY(-1 * size)
+                    successView.setY(-1 * size + UIConstants.navbarHeight)
                     }, completion: nil)
         }
         return successView

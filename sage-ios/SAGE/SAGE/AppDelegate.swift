8bbfb4c44b53bafb57953ba5d0e1c9790c16c3be
Andrew Millman <andrewmillman35@gmail.com>
2016-05-03 16:34:27 -0700

    Refactor common notification code to function

--- sage-ios/SAGE/SAGE/AppDelegate.swift
+++ sage-ios/SAGE/SAGE/AppDelegate.swift
@@ -33,25 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         //Handle push notifications
         if let userInfo = launchOptions?[UIApplicationLaunchOptionsRemoteNotificationKey] as? [String: AnyObject] {
-
-            let objectString = userInfo[PushNotificationConstants.kObject] as! String
-            let notificationType = userInfo[PushNotificationConstants.kType] as! Int
-
-            switch notificationType {
-            case PushNotificationConstants.kAnnouncementType:
-                let announcementJSON = objectString.convertToDictionary()![PushNotificationConstants.kAnnouncement] as! [String: AnyObject]
-                let announcement = Announcement(properties: announcementJSON)
-                RootController.sharedController().handleNewAnnouncement(announcement, applicationState: application.applicationState, launching: true)
-            case PushNotificationConstants.kCheckInRequestType:
-                let checkInRequestJSON = objectString.convertToDictionary()![PushNotificationConstants.kCheckInRequest] as! [String: AnyObject]
-                let checkInRequest = Checkin(propertyDictionary: checkInRequestJSON)
-            case PushNotificationConstants.kSignUpRequestType:
-                let signUpRequestJSON = objectString.convertToDictionary()![PushNotificationConstants.kAnnouncement] as! [String: AnyObject]
-                let signUpRequest = User(propertyDictionary: signUpRequestJSON)
-            default:
-                break
-            }
-
+            self.handleNotification(userInfo, applicationState: application.applicationState, launching: true)
         }
 
         return true
@@ -89,40 +71,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
-        let objectString = userInfo[PushNotificationConstants.kObject] as! String
-        let notificationType = userInfo[PushNotificationConstants.kType] as! Int
+        self.handleNotification(userInfo, applicationState: application.applicationState, launching: false)
+    }
+
+    //
+    // MARK: - Private Functions
+    //
+    private func handleNotification(notification: [NSObject: AnyObject], applicationState: UIApplicationState, launching: Bool) {
+        let objectString = notification[PushNotificationConstants.kObject] as! String
+        let notificationType = notification[PushNotificationConstants.kType] as! Int
 
         switch notificationType {
         case PushNotificationConstants.kAnnouncementType:
             let announcementJSON = objectString.convertToDictionary()![PushNotificationConstants.kAnnouncement] as! [String: AnyObject]
             let announcement = Announcement(properties: announcementJSON)
-            RootController.sharedController().handleNewAnnouncement(announcement, applicationState: application.applicationState, launching: false)
+            RootController.sharedController().handleNewAnnouncement(announcement, applicationState: applicationState, launching: launching)
         case PushNotificationConstants.kCheckInRequestType:
             let checkInRequestJSON = objectString.convertToDictionary()![PushNotificationConstants.kCheckInRequest] as! [String: AnyObject]
             let checkInRequest = Checkin(propertyDictionary: checkInRequestJSON)
-
-            // App is already in foreground
-            if (application.applicationState == .Active) {
-                //RootController.sharedController().displayAnnouncement(announcement)
-                return;
-            }
-            // App is transitioning from background to foreground
-            //RootController.sharedController().displayAnnouncement(announcement)
         case PushNotificationConstants.kSignUpRequestType:
             let signUpRequestJSON = objectString.convertToDictionary()![PushNotificationConstants.kAnnouncement] as! [String: AnyObject]
             let signUpRequest = User(propertyDictionary: signUpRequestJSON)
-
-            // App is already in foreground
-            if (application.applicationState == .Active) {
-                //RootController.sharedController().displayAnnouncement(announcement)
-                return;
-            }
-            // App is transitioning from background to foreground
-            //RootController.sharedController().displayAnnouncement(announcement)
         default:
             break
         }
-
     }
 }
 

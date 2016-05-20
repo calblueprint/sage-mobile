326915009483ffdbe739bfafa6489133497f653a
Charles Xue <charlesx2013@gmail.com>
2016-05-19 23:55:19 -0400

    Change method name so it's more descriptive

---
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/events/schools/UrlConstants.java
@@ -0,0 +1,11 @@
+package blueprint.com.sage.events.schools;
+
+/**
+ * Created by charlesx on 2/8/16.
+ */
+public class UrlConstants {
+    public static final String HTTP = "https";
+    public static final String HOSTNAME = "sage-rails.herokuapp.com";
+    public static final String API = "api";
+    public static final String VERSION = "v1";
+}

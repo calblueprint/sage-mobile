571873e56c1e066da8a29fdff0666f4cb64816f8
Charles Xue <charlesx2013@gmail.com>
2016-08-23 02:46:51 -0700

    Changing animation time

--- sage-android/Sage/app/src/main/java/blueprint/com/sage/landing/LandingActivity.java
+++ sage-android/Sage/app/src/main/java/blueprint/com/sage/landing/LandingActivity.java
@@ -122,7 +122,7 @@ public class LandingActivity extends AbstractActivity {
 
         mSplashDrawable = (GifDrawable) mLandingSplash.getDrawable();
         mSplashDrawable.stop();
-        mSplashDrawable.setSpeed(1.75f);
+        mSplashDrawable.setSpeed(5f);
         mTotalAnimationTime = mSplashDrawable.getDuration();
     }
 
@@ -141,7 +141,7 @@ public class LandingActivity extends AbstractActivity {
 
     private void startAnimation(final Class<?> cls) {
         Animation fadeOutAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_out);
-        fadeOutAnimation.setDuration(500);
+        fadeOutAnimation.setDuration(400);
         fadeOutAnimation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -164,11 +164,10 @@ public class LandingActivity extends AbstractActivity {
                                         startActivity(cls);
                                     }
                                 });
-
                     }
                 };
 
-                mTimer.schedule(timerTask, mTotalAnimationTime);
+                mTimer.schedule(timerTask, mTotalAnimationTime / 4);
             }
 
             @Override

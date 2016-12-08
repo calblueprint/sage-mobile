110493d568d23e1817c55772233057508dcccad2
Andrew Millman <andrewmillman35@gmail.com>
2016-12-08 03:31:51 -0800

    Add API keys + permissions

--- sage-ios/SAGE/SAGE/AppDelegate.swift
+++ sage-ios/SAGE/SAGE/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         GMSServices.provideAPIKey(APIKeys.googleMaps)
+        GMSPlacesClient.provideAPIKey(APIKeys.googleMaps)
         
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.rootViewController = RootController()
